aa0dc2d29b26fadc61c0485316333a4379ad473e
Inwerwm <saclainvervm@gmail.com>
2020-03-11 17:02:15 +0900

    Pmx系クラスに未実装コンストラクタを記述

--- MikuMikuMethods/Pmx.cs
+++ MikuMikuMethods/Pmx.cs
@@ -1,24 +1,79 @@
-﻿using SlimDX;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace MikuMikuMethods
 {
     namespace Pmx
     {
         class Pmx { }
-        class Body { }
-        class Bone { }
-        class Node { }
-        class Header { }
-        class Joint { }
-        class Material { }
-        class ModelInfo { }
-        class Morph { }
-        class SoftBody { }
-        class Vertex { }
+        class Body
+        {
+            public Body()
+            {
+                throw new NotImplementedException("Pmx.Bodyクラスは未実装です");
+            }
+        }
+        class Bone
+        {
+            public Bone()
+            {
+                throw new NotImplementedException("Pmx.Boneクラスは未実装です");
+            }
+        }
+        class Node
+        {
+            public Node()
+            {
+                throw new NotImplementedException("Pmx.Nodeクラスは未実装です");
+            }
+        }
+        class Header
+        {
+            public Header()
+            {
+                throw new NotImplementedException("Pmx.Headerクラスは未実装です");
+            }
+        }
+        class Joint
+        {
+            public Joint()
+            {
+                throw new NotImplementedException("Pmx.Jointクラスは未実装です");
+            }
+        }
+        class Material
+        {
+            public Material()
+            {
+                throw new NotImplementedException("Pmx.Materialクラスは未実装です");
+            }
+        }
+        class ModelInfo
+        {
+            public ModelInfo()
+            {
+                throw new NotImplementedException("Pmx.ModelInfoクラスは未実装です");
+            }
+        }
+        class Morph
+        {
+            public Morph()
+            {
+                throw new NotImplementedException("Pmx.Morphクラスは未実装です");
+            }
+        }
+        class SoftBody
+        {
+            public SoftBody()
+            {
+                throw new NotImplementedException("Pmx.SoftBodyクラスは未実装です");
+            }
+        }
+        class Vertex
+        {
+            public Vertex()
+            {
+                throw new NotImplementedException("Pmx.Vertexクラスは未実装です");
+            }
+        }
     }
 }
