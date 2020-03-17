03dffc5671073821ccbc1b38dfb0b707f18f0f50
Inwerwm <saclainvervm@gmail.com>
2020-03-18 04:41:23 +0900

    拡張メソッドを追加

---
+++ MikuMikuMethods/ExtensionMethods.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+namespace MikuMikuMethods
+{
+    public static class ExtensionMethods
+    {
+        /// <summary>
+        /// 指定された開区間の範囲内であるかを判断します。
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="lower">下限</param>
+        /// <param name="upper">上限</param>
+        /// <returns></returns>
+        public static bool IsWithin<T>(this T i, T lower, T upper) where T : IComparable
+        {
+            if (upper.CompareTo(lower) < 0)
+                throw new ArgumentOutOfRangeException("下限値が上限値よりも大きいです。");
+            return i.CompareTo(lower) * upper.CompareTo(i) > 0;
+        }
+
+        /// <summary>
+        /// 指定された閉区間の範囲内であるかを判断します。
+        /// </summary>
+        /// <param name="i"></param>
+        /// <param name="lower">下限</param>
+        /// <param name="upper">上限</param>
+        /// <returns></returns>
+        public static bool IsInclude<T>(this T i, T lower, T upper) where T : IComparable
+        {
+            if (upper.CompareTo(lower) < 0)
+                throw new ArgumentOutOfRangeException("下限値が上限値よりも大きいです。");
+            return i.CompareTo(lower) * upper.CompareTo(i) >= 0;
+        }
+    }
+}
