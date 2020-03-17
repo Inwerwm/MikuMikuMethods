08f7ceb09ff30266b39141ec4d1a8eae966000fb
Inwerwm <saclainvervm@gmail.com>
2020-03-18 06:43:39 +0900

    拡張メソッドに範囲判定を追加

--- MikuMikuMethods/ExtensionMethods.cs
+++ MikuMikuMethods/ExtensionMethods.cs
@@ -5,7 +5,7 @@ namespace MikuMikuMethods
     public static class ExtensionMethods
     {
         /// <summary>
-        /// 指定された開区間の範囲内であるかを判断します。
+        /// 指定された閉区間の範囲内であるかを判断します。
         /// </summary>
         /// <param name="i"></param>
         /// <param name="lower">下限</param>
@@ -15,21 +15,21 @@ namespace MikuMikuMethods
         {
             if (upper.CompareTo(lower) < 0)
                 throw new ArgumentOutOfRangeException("下限値が上限値よりも大きいです。");
-            return i.CompareTo(lower) * upper.CompareTo(i) > 0;
+            return i.CompareTo(lower) * upper.CompareTo(i) >= 0;
         }
 
         /// <summary>
-        /// 指定された閉区間の範囲内であるかを判断します。
+        /// 指定された開区間の範囲内であるかを判断します。
         /// </summary>
         /// <param name="i"></param>
         /// <param name="lower">下限</param>
         /// <param name="upper">上限</param>
         /// <returns></returns>
-        public static bool IsInclude<T>(this T i, T lower, T upper) where T : IComparable
+        public static bool IsInside<T>(this T i, T lower, T upper) where T : IComparable
         {
             if (upper.CompareTo(lower) < 0)
                 throw new ArgumentOutOfRangeException("下限値が上限値よりも大きいです。");
-            return i.CompareTo(lower) * upper.CompareTo(i) >= 0;
+            return i.CompareTo(lower) * upper.CompareTo(i) > 0;
         }
     }
 }
