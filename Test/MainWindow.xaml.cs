97b8c1d4300c9421301d96df27ab862c03438f90
Inwerwm <saclainvervm@gmail.com>
2020-03-18 06:43:54 +0900

    テスト用プロジェクトを作成

---
+++ Test/MainWindow.xaml.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Windows;
+using MikuMikuMethods;
+
+namespace Test
+{
+    /// <summary>
+    /// MainWindow.xaml の相互作用ロジック
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        MainViewModel mainViewModel;
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            mainViewModel = new MainViewModel();
+            DataContext = mainViewModel;
+        }
+
+        private void Log(string message)
+        {
+            mainViewModel.Log += message + Environment.NewLine;
+        }
+
+        private void Log<T>(string name, T value)
+        {
+            mainViewModel.Log += name + " = " + value + Environment.NewLine;
+        }
+
+        private void ButtonRun_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
---
+++ Test/ViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+
+namespace Test
+{
+    class MainViewModel : INotifyPropertyChanged
+    {
+        public event PropertyChangedEventHandler PropertyChanged = null;
+
+        string log;
+        public string Log { get => log; set { log = value; OnPropertyChanged(nameof(Log)); } }
+
+        protected void OnPropertyChanged(string info)
+        {
+            PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(info));
+        }
+    }
+}
