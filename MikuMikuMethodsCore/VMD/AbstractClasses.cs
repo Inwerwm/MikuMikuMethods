fc3b1fd20244e1cc8c40f1a518db3ad06dcc15fc
Inwerwm <saclainvervm@gmail.com>
2020-11-14 15:12:47 +0900

    VMDのインターフェイスと抽象クラスを作成

---
+++ MikuMikuMethodsCore/VMD/AbstractClasses.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.VMD
+{
+    public abstract class VocaloidFrame : IVocaloidFrame
+    {
+        public abstract FrameType FrameType { get; }
+        public string Name { get; set; }
+        public uint Frame { get; set; }
+        public abstract bool IsCameraType { get; }
+        public abstract bool IsModelType { get; }
+
+        public int CompareTo(IVocaloidFrame other) => Frame.CompareTo(other.Frame);
+        public abstract void Read(BinaryReader reader);
+        public abstract void Write(BinaryWriter writer);
+    }
+
+    public abstract class VocaloidModelTypeFrame : VocaloidFrame
+    {
+        public override bool IsCameraType => false;
+
+        public override bool IsModelType => true;
+    }
+
+    public abstract class VocaloidCameraTypeFrame : VocaloidFrame
+    {
+        public override bool IsCameraType => true;
+
+        public override bool IsModelType => false;
+    }
+}
---
+++ MikuMikuMethodsCore/VMD/Interfaces.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.VMD
+{
+    public enum FrameType
+    {
+        /// <summary>
+        /// カメラ
+        /// </summary>
+        Camera,
+        /// <summary>
+        /// 照明
+        /// </summary>
+        Light,
+        /// <summary>
+        /// 影
+        /// </summary>
+        Shadow,
+        /// <summary>
+        /// 表示・IK
+        /// </summary>
+        Property,
+        /// <summary>
+        /// モーフ
+        /// </summary>
+        Morph,
+        /// <summary>
+        /// モーション
+        /// </summary>
+        Motion,
+    }
+
+    public interface IVocaloidFrame:IComparable<IVocaloidFrame>
+    {
+        FrameType FrameType { get; }
+        string Name { get; set; }
+        uint Frame { get; set; }
+
+        bool IsCameraType { get; }
+        bool IsModelType { get; }
+
+        void Read(BinaryReader reader);
+        void Write(BinaryWriter writer);
+    }
+}
