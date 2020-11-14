3e6e7b81e515c4c82b6dff41fdc709104ae5ce6a
Inwerwm <saclainvervm@gmail.com>
2020-11-14 16:31:19 +0900

    フレームクラスの概形を作成

---
+++ MikuMikuMethodsCore/VMD/CameraTypeFrameClasses.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.VMD
+{
+    /// <summary>
+    /// カメラフレーム
+    /// </summary>
+    class VocaloidCameraFrame : VocaloidCameraTypeFrame
+    {
+        public override FrameType FrameType => VMD.FrameType.Camera;
+
+        public override void Read(BinaryReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// 照明フレーム
+    /// </summary>
+    class VocaloidLightFrame : VocaloidCameraTypeFrame
+    {
+        public override FrameType FrameType => FrameType.Light;
+
+        public override void Read(BinaryReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// 影フレーム
+    /// </summary>
+    class VocaloidShadowFrame : VocaloidCameraTypeFrame
+    {
+        public override FrameType FrameType => FrameType.Shadow;
+
+        public override void Read(BinaryReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
---
+++ MikuMikuMethodsCore/VMD/ModelTypeFrameClasses.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.VMD
+{
+    /// <summary>
+    /// 表示・IKフレーム
+    /// </summary>
+    class VocaloidPropertyFrame : VocaloidModelTypeFrame
+    {
+        public override FrameType FrameType => FrameType.Property;
+
+        public override void Read(BinaryReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// モーフフレーム
+    /// </summary>
+    class VocaloidMorphFrame : VocaloidModelTypeFrame
+    {
+        public override FrameType FrameType => FrameType.Morph;
+
+        public override void Read(BinaryReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+    /// <summary>
+    /// モーションフレーム
+    /// </summary>
+    class VocaloidMotionFrame : VocaloidModelTypeFrame
+    {
+        public override FrameType FrameType => FrameType.Motion;
+
+        public override void Read(BinaryReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        public override void Write(BinaryWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
