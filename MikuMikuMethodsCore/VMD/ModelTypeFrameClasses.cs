96d3bb912a5c82c1b948117f38c47d7c6d8e8ec9
Inwerwm <saclainvervm@gmail.com>
2020-11-17 11:10:21 +0900

    クラスのpublic化+モーションフレームのプロパティを追加

--- MikuMikuMethodsCore/VMD/ModelTypeFrameClasses.cs
+++ MikuMikuMethodsCore/VMD/ModelTypeFrameClasses.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,7 @@ namespace MikuMikuMethods.VMD
     /// <summary>
     /// 表示・IKフレーム
     /// </summary>
-    class VocaloidPropertyFrame : VocaloidModelTypeFrame
+    public class VocaloidPropertyFrame : VocaloidModelTypeFrame
     {
         public override FrameType FrameType => FrameType.Property;
 
@@ -28,7 +29,7 @@ namespace MikuMikuMethods.VMD
     /// <summary>
     /// モーフフレーム
     /// </summary>
-    class VocaloidMorphFrame : VocaloidModelTypeFrame
+    public class VocaloidMorphFrame : VocaloidModelTypeFrame
     {
         public override FrameType FrameType => FrameType.Morph;
 
@@ -46,9 +47,35 @@ namespace MikuMikuMethods.VMD
     /// <summary>
     /// モーションフレーム
     /// </summary>
-    class VocaloidMotionFrame : VocaloidModelTypeFrame
+    public class VocaloidMotionFrame : VocaloidModelTypeFrame
     {
+        /// <summary>
+        /// フレームの種類
+        /// </summary>
         public override FrameType FrameType => FrameType.Motion;
+
+        /// <summary>
+        /// 移動量
+        /// </summary>
+        public Vector3 Trans { get; set; }
+        /// <summary>
+        /// 回転量
+        /// </summary>
+        public Quaternion Rot { get; set; }
+
+        /// <summary>
+        /// 補間曲線
+        /// </summary>
+        public Dictionary<InterpolationItem,InterpolationCurve> InterpolationCurves { get; init; }
+
+        public VocaloidMotionFrame()
+        {
+            InterpolationCurves = new();
+            InterpolationCurves.Add(InterpolationItem.XMove, new());
+            InterpolationCurves.Add(InterpolationItem.YMove, new());
+            InterpolationCurves.Add(InterpolationItem.ZMove, new());
+            InterpolationCurves.Add(InterpolationItem.Rotation, new());
+        }
 
         public override void Read(BinaryReader reader)
         {
