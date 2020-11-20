39e56532ae69e98f18634e1e74db199ac36b2bf1
Inwerwm <saclainvervm@gmail.com>
2020-11-20 11:25:58 +0900

    Pmxモデルクラスのプロパティを作成

--- MikuMikuMethodsCore/PMX/PmxModel.cs
+++ MikuMikuMethodsCore/PMX/PmxModel.cs
@@ -13,6 +13,48 @@ namespace MikuMikuMethods.PMX
     public class PmxModel : IPmxData
     {
         /// <summary>
+        /// ヘッダ
+        /// </summary>
+        public PmxHeader Header { get; } = new();
+        /// <summary>
+        /// モデル情報
+        /// </summary>
+        public PmxModelInfo ModelInfo { get; } = new();
+
+        /// <summary>
+        /// 剛体
+        /// </summary>
+        public List<PmxBody> Bodies { get; } = new();
+        /// <summary>
+        /// ボーン
+        /// </summary>
+        public List<PmxBone> Bones { get; } = new();
+        /// <summary>
+        /// 面
+        /// </summary>
+        public List<PmxFace> Faces { get; }
+        /// <summary>
+        /// ジョイント
+        /// </summary>
+        public List<PmxJoint> Joints { get; } = new();
+        /// <summary>
+        /// 材質
+        /// </summary>
+        public List<PmxMaterial> Materials { get; } = new();
+        /// <summary>
+        /// モーフ
+        /// </summary>
+        public List<PmxMorph> Morphs { get; } = new();
+        /// <summary>
+        /// 表情枠
+        /// </summary>
+        public List<PmxNode> Nodes { get; } = new();
+        /// <summary>
+        /// 頂点
+        /// </summary>
+        public List<PmxVertex> Vertices { get; } = new();
+
+        /// <summary>
         /// データをバイナリから読み込む
         /// </summary>
         /// <param name="reader">読み込み対象のリーダー</param>
