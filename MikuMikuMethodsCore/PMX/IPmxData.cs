11df7d4d276eddee98de9a540a4bd516b2dec5de
Inwerwm <saclainvervm@gmail.com>
2020-11-20 10:46:36 +0900

    PMX系クラスを作成

---
+++ MikuMikuMethodsCore/PMX/IPmxData.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.PMX
+{
+    /// <summary>
+    /// PMX関連のインターフェイス
+    /// </summary>
+    public interface IPmxData
+    {
+        /// <summary>
+        /// データをバイナリから読み込む
+        /// </summary>
+        /// <param name="reader">読み込み対象のリーダー</param>
+        public void Read(BinaryReader reader);
+
+        /// <summary>
+        /// データをバイナリで書き込む
+        /// </summary>
+        /// <param name="writer">書き込み対象のライター</param>
+        public void Write(BinaryWriter writer);
+    }
+}
---
+++ MikuMikuMethodsCore/PMX/PmxJoint.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.PMX
+{
+    /// <summary>
+    /// ジョイント
+    /// </summary>
+    public class PmxJoint:IPmxData
+    {
+        /// <summary>
+        /// データをバイナリから読み込む
+        /// </summary>
+        /// <param name="reader">読み込み対象のリーダー</param>
+        public void Read(BinaryReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// データをバイナリで書き込む
+        /// </summary>
+        /// <param name="writer">書き込み対象のライター</param>
+        public void Write(BinaryWriter writer)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
