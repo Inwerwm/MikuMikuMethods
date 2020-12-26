a470a9ca1a0c29fb952e2df38ce3375cc526828c
Inwerwm <saclainvervm@gmail.com>
2020-12-26 21:46:31 +0900

    ドキュメントコメントを追加

--- MikuMikuMethodsCore/Utility/Encoding.cs
+++ MikuMikuMethodsCore/Utility/Encoding.cs
@@ -7,19 +7,26 @@ using System.Threading.Tasks;
 namespace MikuMikuMethods
 {
     /// <summary>
-    /// <para>MMD関係で使用するエンコードのアダプタクラス</para>
-    /// <para>Shift JIS がそのままだと使えないので作った</para>
+    /// <para>MMD関係で使用するエンコード方式</para>
+    /// <para>Shift JIS はそのままだと使えないので</para>
     /// </summary>
     public static class Encoding
     {
+        /// <summary>
+        /// ShiftJISを使うための初期設定が済んでいるか
+        /// </summary>
         private static bool ProviderIsRegistered { get; set; }
 
+        /// <summary>
+        /// 日本語 Shift JIS
+        /// </summary>
         public static System.Text.Encoding ShiftJIS
         {
             get
             {
                 if (!ProviderIsRegistered)
                 {
+                    // Shift JIS を使うためには初期設定が必要
                     System.Text.Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
                     ProviderIsRegistered = true;
                 }
@@ -27,8 +34,14 @@ namespace MikuMikuMethods
             }
         }
 
+        /// <summary>
+        /// UTF-16
+        /// </summary>
         public static System.Text.Encoding Unicode => System.Text.Encoding.Unicode;
 
+        /// <summary>
+        /// UTF-8
+        /// </summary>
         public static System.Text.Encoding UTF8 => System.Text.Encoding.UTF8;
     }
 }
