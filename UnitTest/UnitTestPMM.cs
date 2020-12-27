402126e9959ae1cd2143e97a3723c8510771d54c
Inwerwm <saclainvervm@gmail.com>
2020-12-27 23:33:35 +0900

    PmmModel用テストを分割

--- UnitTest/UnitTestPMM.cs
+++ UnitTest/UnitTestPMM.cs
@@ -34,6 +34,11 @@ namespace UnitTest
             Assert.AreEqual("Polygon Movie maker 0002", pmm.Version);
             Assert.IsTrue(pmm.EditorState.IsCameraMode);
             Assert.AreEqual(1, pmm.Models.Count);
+        }
+
+        [TestMethod]
+        public void Test_PmmModel()
+        {
             Assert.AreEqual("ルベシア・シェリングヴェーヌ", pmm.Models[0].Name);
             Assert.AreEqual("Rybecia Sherringvaine", pmm.Models[0].NameEn);
             Assert.AreEqual(@"C:\MMD\_モデル(人物)\_quappa-el\EL-D2M-Rybecia_Shader\ルベシア.pmx", pmm.Models[0].Path);
@@ -41,6 +46,5 @@ namespace UnitTest
             Assert.IsNull(pmm.Models[0].InitialBoneFrames[0].Index);
             Assert.AreEqual(1, pmm.Models[0].InitialBoneFrames[0].Offset.Y);
         }
-
     }
 }
