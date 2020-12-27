e4486eeed2c1b643f3945ecacbd4d74fb0b779a8
Inwerwm <saclainvervm@gmail.com>
2020-12-27 22:59:33 +0900

    テストデータの読み込みを分離

--- UnitTest/UnitTestPMM.cs
+++ UnitTest/UnitTestPMM.cs
@@ -12,24 +12,35 @@ namespace UnitTest
     [TestClass]
     public class UnitTestPMM
     {
-        [TestMethod]
-        public void Test_PolygonMovieMaker()
+        PolygonMovieMaker pmm = new();
+
+        private void LoadTestData()
         {
-            PolygonMovieMaker pmm = new();
-            using(FileStream stream = new("TestData/testProject.pmm", FileMode.Open))
-            using(BinaryReader reader = new(stream, MikuMikuMethods.Encoding.ShiftJIS))
+            using (FileStream stream = new("TestData/testProject.pmm", FileMode.Open))
+            using (BinaryReader reader = new(stream, MikuMikuMethods.Encoding.ShiftJIS))
             {
                 pmm.Read(reader);
             }
+        }
 
+        public UnitTestPMM()
+        {
+            LoadTestData();
+        }
+
+        [TestMethod]
+        public void Test_PolygonMovieMaker()
+        {
             Assert.AreEqual("Polygon Movie maker 0002", pmm.Version);
             Assert.IsTrue(pmm.EditorState.IsCameraMode);
             Assert.AreEqual(1, pmm.Models.Count);
             Assert.AreEqual("ルベシア・シェリングヴェーヌ", pmm.Models[0].Name);
             Assert.AreEqual("Rybecia Sherringvaine", pmm.Models[0].NameEn);
+            Assert.AreEqual(@"C:\MMD\_モデル(人物)\_quappa-el\EL-D2M-Rybecia_Shader\ルベシア.pmx", pmm.Models[0].Path);
             Assert.AreEqual(16, pmm.Models[0].FrameEditor.RowCount);
             Assert.IsNull(pmm.Models[0].InitialBoneFrames[0].Index);
             Assert.AreEqual(1, pmm.Models[0].InitialBoneFrames[0].Offset.Y);
         }
+
     }
 }
