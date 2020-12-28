c9a59c4352f9acfc3ddf0f265c71400a2e556b09
Inwerwm <saclainvervm@gmail.com>
2020-12-28 15:29:06 +0900

    表示・IK・外観フレーム情報と未確定編集情報と描画設定の読込処理を実装

--- MikuMikuMethodsCore/PMM/PmmModel.cs
+++ MikuMikuMethodsCore/PMM/PmmModel.cs
@@ -1,6 +1,7 @@
 ﻿using MikuMikuMethods.Extension;
 using System.Collections.Generic;
 using System.IO;
+using System.Numerics;
 
 namespace MikuMikuMethods.PMM
 {
@@ -30,21 +31,19 @@ namespace MikuMikuMethods.PMM
         public string Path { get; set; }
 
         /// <summary>
-        /// 表示
+        /// 描画設定
         /// </summary>
-        public bool Visible { get; set; }
-
-        /// <summary>
-        /// <para>モデル描画順</para>
-        /// <para>これが狂うとモデルセレクタの選択と実際に動くモデルに齟齬が発生する</para>
-        /// <para>セレクタ一覧が描画順に基づいて作られるためだと思われる</para>
-        /// </summary>
-        public byte RenderOrder { get; set; }
+        public RenderConfig RenderConfig { get; init; }
 
         /// <summary>
         /// キーフレーム編集画面の状態
         /// </summary>
         public FrameEditorState FrameEditor { get; init; }
+
+        /// <summary>
+        /// 未確定編集状態
+        /// </summary>
+        public TemporaryEditState Uncomitted { get; init; }
 
         /// <summary>
         /// ボーン名
@@ -102,11 +101,22 @@ namespace MikuMikuMethods.PMM
         public List<PmmMorphFrame> MorphFrames { get; init; }
 
         /// <summary>
+        /// 初期表示・IK・外観フレーム
+        /// </summary>
+        public PmmConfigFrame InitialConfigFrame { get; set; }
+
+        /// <summary>
+        /// 表示・IK・外観のキーフレーム
+        /// </summary>
+        public List<PmmConfigFrame> ConfigFrames { get; init; }
+
+        /// <summary>
         /// コンストラクタ
         /// </summary>
         public PmmModel()
         {
             FrameEditor = new();
+            Uncomitted = new();
             BoneNames = new();
             MorphNames = new();
             IKBoneIndices = new();
@@ -115,6 +125,7 @@ namespace MikuMikuMethods.PMM
             BoneFrames = new();
             InitialMorphFrames = new();
             MorphFrames = new();
+            ConfigFrames = new();
         }
 
         /// <summary>
@@ -129,6 +140,8 @@ namespace MikuMikuMethods.PMM
             NameEn = reader.ReadString();
             Path = reader.ReadString(256, Encoding.ShiftJIS, '\0');
 
+            // キーフレームエディタの行数
+            // 表情枠数から求まるので破棄
             _ = reader.ReadByte();
 
             var boneCount = reader.ReadInt32();
@@ -155,9 +168,9 @@ namespace MikuMikuMethods.PMM
                 ParentableBoneIndices.Add(reader.ReadInt32());
             }
 
-            RenderOrder = reader.ReadByte();
+            RenderConfig.RenderOrder = reader.ReadByte();
 
-            Visible = reader.ReadBoolean();
+            Uncomitted.Visible = reader.ReadBoolean();
             SelectedBoneIndex = reader.ReadInt32();
             SelectedMorphIndices = (reader.ReadInt32(), reader.ReadInt32(), reader.ReadInt32(), reader.ReadInt32());
 
@@ -199,6 +212,58 @@ namespace MikuMikuMethods.PMM
                 morphFrame.Read(reader, reader.ReadInt32());
                 MorphFrames.Add(morphFrame);
             }
+
+            {
+                PmmConfigFrame configFrame = new();
+                configFrame.Read(reader, null, ikCount, parentableBoneCount);
+                InitialConfigFrame = configFrame;
+            }
+
+            var configFrameCount = reader.ReadInt32();
+            for (int i = 0; i < configFrameCount; i++)
+            {
+                PmmConfigFrame configFrame = new();
+                configFrame.Read(reader, null, ikCount, parentableBoneCount);
+                ConfigFrames.Add(configFrame);
+            }
+
+            for (int i = 0; i < boneCount; i++)
+            {
+                TemporaryBoneState uncomittedBone = new();
+                uncomittedBone.Offset = reader.ReadVector3();
+                uncomittedBone.Rotate = reader.ReadQuaternion();
+                uncomittedBone.IsThis = reader.ReadBoolean();
+                uncomittedBone.EnablePhysic = reader.ReadBoolean();
+                uncomittedBone.RowIsSelected = reader.ReadBoolean();
+
+                Uncomitted.Bones.Add(uncomittedBone);
+            }
+
+            for (int i = 0; i < morphCount; i++)
+            {
+                Uncomitted.MorphWeights.Add(reader.ReadSingle());
+            }
+
+            for (int i = 0; i < ikCount; i++)
+            {
+                Uncomitted.IKEnable.Add(reader.ReadBoolean());
+            }
+
+            for (int i = 0; i < parentableBoneCount; i++)
+            {
+                TemporaryParentSetting uncomittedParentSetting = new();
+                uncomittedParentSetting.StartFrame = reader.ReadInt32();
+                uncomittedParentSetting.EndFrame = reader.ReadInt32();
+                uncomittedParentSetting.ModelIndex = reader.ReadInt32();
+                uncomittedParentSetting.BoneIndex = reader.ReadInt32();
+
+                Uncomitted.ParentSettings.Add(uncomittedParentSetting);
+            }
+
+            RenderConfig.EnableAlphaBlend = reader.ReadBoolean();
+            RenderConfig.EdgeWidth = reader.ReadSingle();
+            RenderConfig.EnableSelfShadow = reader.ReadBoolean();
+            RenderConfig.CalculateOrder = reader.ReadByte();
         }
     }
 
@@ -235,5 +300,134 @@ namespace MikuMikuMethods.PMM
         {
             DoesOpenNode = new();
         }
+    }
+
+    /// <summary>
+    /// 描画設定
+    /// </summary>
+    public class RenderConfig
+    {
+        /// <summary>
+        /// <para>モデル描画順</para>
+        /// <para>これが狂うとモデルセレクタの選択と実際に動くモデルに齟齬が発生する</para>
+        /// <para>セレクタ一覧が描画順に基づいて作られるためだと思われる</para>
+        /// </summary>
+        public byte RenderOrder { get; set; }
+
+        /// <summary>
+        /// モデル計算順
+        /// </summary>
+        public byte CalculateOrder { get; set; }
+
+        /// <summary>
+        /// エッジ幅
+        /// </summary>
+        public float EdgeWidth { get; set; }
+
+        /// <summary>
+        /// 加算合成が有効か
+        /// </summary>
+        public bool EnableAlphaBlend { get; set; }
+        /// <summary>
+        /// セルフ影が有効か
+        /// </summary>
+        public bool EnableSelfShadow { get; set; }
+    }
+
+    /// <summary>
+    /// 未確定の編集状態
+    /// </summary>
+    public class TemporaryEditState
+    {
+        /// <summary>
+        /// 表示
+        /// </summary>
+        public bool Visible { get; set; }
+
+        /// <summary>
+        /// ボーンのパラメータ
+        /// </summary>
+        public List<TemporaryBoneState> Bones { get; init; }
+
+        /// <summary>
+        /// モーフの係数
+        /// </summary>
+        public List<float> MorphWeights { get; init; }
+
+        /// <summary>
+        /// IKが有効か
+        /// </summary>
+        public List<bool> IKEnable { get; init; }
+
+        /// <summary>
+        /// 外部親設定
+        /// </summary>
+        public List<TemporaryParentSetting> ParentSettings { get; init; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public TemporaryEditState()
+        {
+            Bones = new();
+            MorphWeights = new();
+            IKEnable = new();
+            ParentSettings = new();
+        }
+    }
+
+    /// <summary>
+    /// 未確定の編集中ボーン情報
+    /// </summary>
+    public class TemporaryBoneState
+    {
+        /// <summary>
+        /// 移動量
+        /// </summary>
+        public Vector3 Offset { get; set; }
+
+        /// <summary>
+        /// 回転量
+        /// </summary>
+        public Quaternion Rotate { get; set; }
+
+        /// <summary>
+        /// 物理が有効か
+        /// </summary>
+        public bool EnablePhysic { get; set; }
+
+        /// <summary>
+        /// 現在選択状態か
+        /// </summary>
+        public bool RowIsSelected { get; set; }
+
+        /// <summary>
+        /// 未確定ボーンか
+        /// </summary>
+        public bool IsThis { get; set; }
+    }
+
+    /// <summary>
+    /// 未確定の外部親設定
+    /// </summary>
+    public class TemporaryParentSetting
+    {
+        /// <summary>
+        /// 外部親設定がされたフレーム番号
+        /// </summary>
+        public int StartFrame { get; set; }
+        /// <summary>
+        /// 外部親設定が変化するフレーム番号
+        /// </summary>
+        public int EndFrame { get; set; }
+
+        /// <summary>
+        /// 外部親のモデルインデックス
+        /// </summary>
+        public int ModelIndex { get; set; }
+        /// <summary>
+        /// 外部親のボーンインデックス
+        /// </summary>
+        public int BoneIndex { get; set; }
     }
 }
