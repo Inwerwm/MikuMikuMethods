4bb330a1517e2081eaad8bc53ec3bec36accf6a7
Inwerwm <saclainvervm@gmail.com>
2021-01-01 23:14:56 +0900

    VMDの入出力テストを追加

---
+++ UnitTest/UnitTestVMD.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MikuMikuMethods.VMD;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [TestClass]
+    public class UnitTestVMD
+    {
+        [TestMethod]
+        public void Test_VocaloidMotionData()
+        {
+            using (FileStream sourceFile = new FileStream(@"TestData\Number Nine.vmd", FileMode.Open))
+            using (FileStream targetFile = new(@"TestData\result.vmd", FileMode.OpenOrCreate))
+            using (BinaryReader reader = new(sourceFile))
+            using (BinaryWriter writer = new(targetFile))
+            {
+                VocaloidMotionData vmd = new(reader);
+                vmd.Write(writer);
+            }
+        }
+    }
+}
