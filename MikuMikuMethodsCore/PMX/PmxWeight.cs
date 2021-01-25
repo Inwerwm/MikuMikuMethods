d13d01f310caef60eb3cfae9d118ed793c623c9a
Inwerwm <saclainvervm@gmail.com>
2021-01-25 17:03:40 +0900

    ウェイト情報クラスと変形種列挙を追加

---
+++ MikuMikuMethodsCore/PMX/PmxWeight.cs
@@ -0,0 +1,29 @@
+﻿namespace MikuMikuMethods.PMX
+{
+    /// <summary>
+    /// ウェイト変形方式
+    /// </summary>
+    public enum PmxWeightType
+    {
+        BDEF1,
+        BDEF2,
+        BDEF4,
+        SDEF,
+    }
+
+    /// <summary>
+    /// ウェイト値
+    /// </summary>
+    public record PmxWeight
+    {
+        /// <summary>
+        /// 関連ボーン
+        /// </summary>
+        public PmxBone Bone { get; set; }
+
+        /// <summary>
+        /// ウェイト値
+        /// </summary>
+        public float Value { get; set; }
+    }
+}
