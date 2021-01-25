2dee0f0b8dda52b3404b61b7f5c1c56dde7185bd
Inwerwm <saclainvervm@gmail.com>
2021-01-25 18:23:03 +0900

    PmxTextureを追加

---
+++ MikuMikuMethodsCore/PMX/PmxTexture.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.PMX
+{
+    /// <summary>
+    /// テクスチャ情報
+    /// </summary>
+    public record PmxTexture
+    {
+        public string Path { get; set; }
+
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        /// <param name="path">テクスチャのパス</param>
+        public PmxTexture(string path)
+        {
+            Path = path;
+        }
+    }
+}
