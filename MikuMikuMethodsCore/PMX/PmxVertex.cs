7b758a16a36ce4f98fbe29f9ea33cb679c32f737
Inwerwm <saclainvervm@gmail.com>
2021-01-25 17:03:56 +0900

    PMXの頂点の内部表現を実装

--- MikuMikuMethodsCore/PMX/PmxVertex.cs
+++ MikuMikuMethodsCore/PMX/PmxVertex.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,41 @@ namespace MikuMikuMethods.PMX
     /// </summary>
     public class PmxVertex : IPmxData
     {
+        /// <summary>
+        /// 位置
+        /// </summary>
+        public Vector3 Position { get; set; }
+
+        /// <summary>
+        /// 法線
+        /// </summary>
+        public Vector3 Normal { get; set; }
+
+        /// <summary>
+        /// テクスチャのUV座標
+        /// </summary>
+        public Vector2 UV { get; set; }
+
+        /// <summary>
+        /// 追加UVの座標
+        /// </summary>
+        public Vector4[] AdditonalUVs { get; }
+
+        /// <summary>
+        /// ウェイト変形方式
+        /// </summary>
+        public PmxWeightType WeightType { get; set; }
+
+        /// <summary>
+        /// ウェイト値
+        /// </summary>
+        public List<PmxWeight> Weights { get; }
+
+        /// <summary>
+        /// エッジ倍率
+        /// </summary>
+        public float EdgeScale { get; set; }
+
         /// <summary>
         /// データをバイナリから読み込む
         /// </summary>
