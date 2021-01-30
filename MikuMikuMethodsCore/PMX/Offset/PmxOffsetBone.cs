954da48dd600891cdefc77e6c1357185b418f9e8
Inwerwm <saclainvervm@gmail.com>
2021-01-31 00:06:42 +0900

    PmxOffsetの実装

--- MikuMikuMethodsCore/PMX/Offset/PmxOffsetBone.cs
+++ MikuMikuMethodsCore/PMX/Offset/PmxOffsetBone.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,20 @@ namespace MikuMikuMethods.PMX
     /// </summary>
     public class PmxOffsetBone : IPmxOffset
     {
+        /// <summary>
+        /// 対象ボーン
+        /// </summary>
+        public PmxBone Target { get; set; }
+
+        /// <summary>
+        /// 移動量
+        /// </summary>
+        public Vector3 Offset { get; set; }
+        /// <summary>
+        /// 回転量
+        /// </summary>
+        public Quaternion Rotate { get; set; }
+
         public void Read(BinaryReader reader)
         {
             throw new NotImplementedException();
--- MikuMikuMethodsCore/PMX/Offset/PmxOffsetVertex.cs
+++ MikuMikuMethodsCore/PMX/Offset/PmxOffsetVertex.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Numerics;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -12,6 +13,16 @@ namespace MikuMikuMethods.PMX
     /// </summary>
     public class PmxOffsetVertex : IPmxOffset
     {
+        /// <summary>
+        /// 対象頂点
+        /// </summary>
+        public PmxVertex Target { get; set; }
+
+        /// <summary>
+        /// 移動量
+        /// </summary>
+        public Vector3 Offset { get; set; }
+
         public void Read(BinaryReader reader)
         {
             throw new NotImplementedException();
