2a05327f0b63cdbd9518b1ba20556d4969e30e2a
Inwerwm <saclainvervm@gmail.com>
2021-02-01 12:28:27 +0900

    VMDの補間曲線テストを追加

--- UnitTest/UnitTestVMD.cs
+++ UnitTest/UnitTestVMD.cs
@@ -1,4 +1,5 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MikuMikuMethods;
 using MikuMikuMethods.VMD;
 using System;
 using System.Collections.Generic;
@@ -15,13 +16,71 @@ namespace UnitTest
         [TestMethod]
         public void Test_VocaloidMotionData()
         {
-            using (FileStream sourceFile = new FileStream(@"TestData\Number Nine.vmd", FileMode.Open))
-            using (FileStream targetFile = new(@"TestData\result.vmd", FileMode.OpenOrCreate))
-            using (BinaryReader reader = new(sourceFile))
-            using (BinaryWriter writer = new(targetFile))
+            using (BinaryReader reader = new(new FileStream(@"TestData\interpolateTest.vmd", FileMode.Open)))
+            using (BinaryWriter writer = new(new FileStream(@"TestData\interpolateTest_Result.vmd", FileMode.OpenOrCreate)))
             {
                 VocaloidMotionData vmd = new(reader);
                 vmd.Write(writer);
+
+                var curves = vmd.MotionFrames[0].InterpolationCurves;
+
+                var xCurve = curves[InterpolationItem.XPosition];
+                var yCurve = curves[InterpolationItem.YPosition];
+                var zCurve = curves[InterpolationItem.ZPosition];
+                var rCurve = curves[InterpolationItem.Rotation];
+
+                Assert.AreEqual(0x00, xCurve.EarlyControlePoint.X);
+                Assert.AreEqual(0x7F, xCurve.EarlyControlePoint.Y);
+                Assert.AreEqual(0x7F, xCurve.LateControlePoint.X);
+                Assert.AreEqual(0x7F, xCurve.LateControlePoint.Y);
+
+                Assert.AreEqual(0x7F, yCurve.EarlyControlePoint.X);
+                Assert.AreEqual(0x7F, yCurve.EarlyControlePoint.Y);
+                Assert.AreEqual(0x00, yCurve.LateControlePoint.X);
+                Assert.AreEqual(0x7F, yCurve.LateControlePoint.Y);
+
+                Assert.AreEqual(0x7F, zCurve.EarlyControlePoint.X);
+                Assert.AreEqual(0x7F, zCurve.EarlyControlePoint.Y);
+                Assert.AreEqual(0x7F, zCurve.LateControlePoint.X);
+                Assert.AreEqual(0x00, zCurve.LateControlePoint.Y);
+
+                Assert.AreEqual(0x7F, rCurve.EarlyControlePoint.X);
+                Assert.AreEqual(0x00, rCurve.EarlyControlePoint.Y);
+                Assert.AreEqual(0x7F, rCurve.LateControlePoint.X);
+                Assert.AreEqual(0x7F, rCurve.LateControlePoint.Y);
+            }
+
+            using (BinaryReader reader = new(new FileStream(@"TestData\interpolateTest_Result.vmd", FileMode.Open)))
+            {
+                VocaloidMotionData vmd = new(reader);
+
+                var curves = vmd.MotionFrames[0].InterpolationCurves;
+
+                var xCurve = curves[InterpolationItem.XPosition];
+                var yCurve = curves[InterpolationItem.YPosition];
+                var zCurve = curves[InterpolationItem.ZPosition];
+                var rCurve = curves[InterpolationItem.Rotation];
+
+                Assert.AreEqual(0x00, xCurve.EarlyControlePoint.X);
+                Assert.AreEqual(0x7F, xCurve.EarlyControlePoint.Y);
+                Assert.AreEqual(0x7F, xCurve.LateControlePoint.X);
+                Assert.AreEqual(0x7F, xCurve.LateControlePoint.Y);
+
+                Assert.AreEqual(0x7F, yCurve.EarlyControlePoint.X);
+                Assert.AreEqual(0x7F, yCurve.EarlyControlePoint.Y);
+                Assert.AreEqual(0x00, yCurve.LateControlePoint.X);
+                Assert.AreEqual(0x7F, yCurve.LateControlePoint.Y);
+
+                Assert.AreEqual(0x7F, zCurve.EarlyControlePoint.X);
+                Assert.AreEqual(0x7F, zCurve.EarlyControlePoint.Y);
+                Assert.AreEqual(0x7F, zCurve.LateControlePoint.X);
+                Assert.AreEqual(0x00, zCurve.LateControlePoint.Y);
+
+                Assert.AreEqual(0x7F, rCurve.EarlyControlePoint.X);
+                Assert.AreEqual(0x00, rCurve.EarlyControlePoint.Y);
+                Assert.AreEqual(0x7F, rCurve.LateControlePoint.X);
+                Assert.AreEqual(0x7F, rCurve.LateControlePoint.Y);
+
             }
         }
     }
