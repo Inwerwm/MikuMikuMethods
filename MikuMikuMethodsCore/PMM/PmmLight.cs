7e863c647f50cfc17d82317f1cda3631713323dc
Inwerwm <saclainvervm@gmail.com>
2021-04-17 21:18:03 +0900

    Pmm照明関連クラスを生成

---
+++ MikuMikuMethodsCore/PMM/PmmLight.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.PMM
+{
+    /// <summary>
+    /// PMMの照明情報
+    /// </summary>
+    public class PmmLight
+    {
+
+    }
+}
