c86701967d69cd26f8639750cdf19b000c22aba5
Inwerwm <saclainvervm@gmail.com>
2021-04-17 21:23:53 +0900

    照明クラスの概形を記述

--- MikuMikuMethodsCore/PMM/PmmLight.cs
+++ MikuMikuMethodsCore/PMM/PmmLight.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,6 +12,52 @@ namespace MikuMikuMethods.PMM
     /// </summary>
     public class PmmLight
     {
+        /// <summary>
+        /// 初期位置の照明フレーム
+        /// </summary>
+        public PmmLightFrame InitialFrame { get; set; }
+        /// <summary>
+        /// 照明のキーフレーム
+        /// </summary>
+        public List<PmmLightFrame> Frames { get; init; }
+        /// <summary>
+        /// 未確定編集状態
+        /// </summary>
+        public PmmLightFrame Uncomitted { get; set; }
 
+        /// <summary>
+        /// コンストラクタ
+        /// </summary>
+        public PmmLight()
+        {
+            Frames = new();
+        }
+
+        /// <summary>
+        /// バイナリデータから照明情報を読み込み
+        /// </summary>
+        /// <param name="reader">読み込むファイル</param>
+        public PmmLight(BinaryReader reader) : this()
+        {
+            Read(reader);
+        }
+
+        /// <summary>
+        /// バイナリデータから読み込み
+        /// </summary>
+        /// <param name="reader">読み込むファイル</param>
+        public void Read(BinaryReader reader)
+        {
+            throw new NotImplementedException();
+        }
+
+        /// <summary>
+        /// ファイルに書込
+        /// </summary>
+        /// <param name="writer">出力対象バイナリファイル</param>
+        public void Write(BinaryWriter writer)
+        {
+            throw new NotImplementedException();
+        }
     }
 }
