36828dba9318a1b7f7746fc451d0984e83f2ae07
Inwerwm <saclainvervm@gmail.com>
2021-04-22 21:16:11 +0900

    重力関連を実装

--- MikuMikuMethodsCore/PMM/Frame/PmmGravityFrame.cs
+++ MikuMikuMethodsCore/PMM/Frame/PmmGravityFrame.cs
@@ -1,12 +1,92 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using MikuMikuMethods.Extension;
+using System.IO;
+using System.Numerics;
 
 namespace MikuMikuMethods.PMM.Frame
 {
-    class PmmGravityFrame : PmmFrame
+    /// <summary>
+    /// 重力フレーム情報
+    /// </summary>
+    public class PmmGravityFrame : PmmFrame
     {
+
+        /// <summary>
+        /// ノイズ不可On/Off
+        /// </summary>
+        public bool EnableNoize { get; set; }
+        /// <summary>
+        /// ノイズ量
+        /// </summary>
+        public int NoizeAmount { get; set; }
+        /// <summary>
+        /// 加速度
+        /// </summary>
+        public float Acceleration { get; set; }
+        /// <summary>
+        /// 方向
+        /// </summary>
+        public Vector3 Direction { get; set; }
+
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
+        public PmmGravityFrame()
+        {
+            EnableNoize = false;
+            NoizeAmount = 10;
+            Acceleration = 9.8f;
+            Direction = new(0, -1, 0);
+        }
+
+        /// <summary>
+        /// バイナリデータから読み込み
+        /// </summary>
+        /// <param name="reader">読み込むファイル</param>
+        /// <param name="index">フレームID</param>
+        public PmmGravityFrame(BinaryReader reader, int? index)
+        {
+            Read(reader, index);
+        }
+
+        /// <summary>
+        /// バイナリデータから読み込み
+        /// </summary>
+        /// <param name="reader">読み込むファイル</param>
+        /// <param name="index">フレームID</param>
+        public void Read(BinaryReader reader, int? index)
+        {
+            Index = index;
+
+            Frame = reader.ReadInt32();
+            PreviousFrameIndex = reader.ReadInt32();
+            NextFrameIndex = reader.ReadInt32();
+
+            EnableNoize = reader.ReadBoolean();
+            NoizeAmount = reader.ReadInt32();
+            Acceleration = reader.ReadSingle();
+            Direction = reader.ReadVector3();
+
+            IsSelected = reader.ReadBoolean();
+        }
+        /// <summary>
+        /// ファイルに書込
+        /// </summary>
+        /// <param name="writer">出力対象バイナリファイル</param>
+        public void Write(BinaryWriter writer)
+        {
+            if (Index.HasValue)
+                writer.Write(Index.Value);
+
+            writer.Write(Frame);
+            writer.Write(PreviousFrameIndex);
+            writer.Write(NextFrameIndex);
+
+            writer.Write(EnableNoize);
+            writer.Write(NoizeAmount);
+            writer.Write(Acceleration);
+            writer.Write(Direction);
+
+            writer.Write(IsSelected);
+        }
     }
 }
--- MikuMikuMethodsCore/PMM/PmmGravity.cs
+++ MikuMikuMethodsCore/PMM/PmmGravity.cs
@@ -1,12 +1,92 @@
-﻿using System;
+﻿using MikuMikuMethods.Extension;
+using MikuMikuMethods.PMM.Frame;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+using System.IO;
+using System.Numerics;
 
 namespace MikuMikuMethods.PMM
 {
-    class PmmGravity
+    /// <summary>
+    /// PMMの重力情報
+    /// </summary>
+    public class PmmGravity
     {
+        /// <summary>
+        /// 現在のノイズ不可On/Off
+        /// </summary>
+        public bool EnableNoize { get; set; }
+        /// <summary>
+        /// 現在のノイズ量
+        /// </summary>
+        public int NoizeAmount { get; set; }
+        /// <summary>
+        /// 現在の加速度
+        /// </summary>
+        public float Acceleration { get; set; }
+        /// <summary>
+        /// 現在の方向
+        /// </summary>
+        public Vector3 Direction { get; set; }
+
+        /// <summary>
+        /// 初期位置の重力フレーム
+        /// </summary>
+        public PmmGravityFrame InitialFrame { get; set; }
+        /// <summary>
+        /// 重力のキーフレーム
+        /// </summary>
+        public List<PmmGravityFrame> Frames { get; init; }
+
+        /// <summary>
+        /// デフォルトコンストラクタ
+        /// </summary>
+        public PmmGravity()
+        {
+            Frames = new();
+        }
+
+        /// <summary>
+        /// バイナリデータから読み込み
+        /// </summary>
+        /// <param name="reader">読み込むファイル</param>
+        public PmmGravity(BinaryReader reader)
+        {
+            Read(reader);
+        }
+
+        /// <summary>
+        /// バイナリデータから読み込み
+        /// </summary>
+        /// <param name="reader">読み込むファイル</param>
+        public void Read(BinaryReader reader)
+        {
+            Acceleration = reader.ReadSingle();
+            NoizeAmount = reader.ReadInt32();
+            Direction = reader.ReadVector3();
+            EnableNoize = reader.ReadBoolean();
+
+            InitialFrame = new(reader, null);
+
+            var frameCount = reader.ReadInt32();
+            for (int i = 0; i < frameCount; i++)
+                Frames.Add(new(reader, i));
+        }
+        /// <summary>
+        /// ファイルに書込
+        /// </summary>
+        /// <param name="writer">出力対象バイナリファイル</param>
+        public void Write(BinaryWriter writer)
+        {
+            writer.Write(Acceleration);
+            writer.Write(NoizeAmount);
+            writer.Write(Direction);
+            writer.Write(EnableNoize);
+
+            InitialFrame.Write(writer);
+
+            writer.Write(Frames.Count);
+            foreach (var frame in Frames)
+                frame.Write(writer);
+        }
     }
 }
