9cfa23e501b7ce45f422c7c6680b8012a91f872a
Inwerwm <saclainvervm@gmail.com>
2021-04-29 22:06:30 +0900

    差分を見るためjson出力処理を追加

--- UnitTest/UnitTestPMM.cs
+++ UnitTest/UnitTestPMM.cs
@@ -4,6 +4,7 @@ using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.Serialization.Json;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -17,7 +18,6 @@ namespace UnitTest
         public UnitTestPMM()
         {
             testPmm = new();
-
         }
 
         [TestMethod]
@@ -26,7 +26,7 @@ namespace UnitTest
             using (FileStream stream = new("TestData/testProject.pmm", FileMode.Open))
             using (BinaryReader reader = new(stream, MikuMikuMethods.Encoding.ShiftJIS))
             using (FileStream outStream = new("TestData/output.pmm", FileMode.Create))
-            using (BinaryWriter writer = new(stream, MikuMikuMethods.Encoding.ShiftJIS))
+            using (BinaryWriter writer = new(outStream, MikuMikuMethods.Encoding.ShiftJIS))
             {
                 testPmm.Read(reader);
                 testPmm.Write(writer);
@@ -37,11 +37,22 @@ namespace UnitTest
 
                 using(BinaryReader outReader = new(outStream, MikuMikuMethods.Encoding.ShiftJIS))
                 {
-                    Assert.AreEqual(reader.BaseStream.Length, outReader.BaseStream.Length);
+                    var outPmm = new PolygonMovieMaker(outReader);
 
-                    for (int i = 0; reader.BaseStream.Position < reader.BaseStream.Length; i++)
+                    //もとのPMMと書込読込PMMのインスタンスをシリアライズして
+                    //テキストで差分を見れるようにする
+                    using (FileStream inJson = new("TestData/originPmm.json", FileMode.Create))
+                    using(FileStream outJson = new("TestData/outputPmm.json", FileMode.Create))
                     {
-                        Assert.AreEqual(reader.ReadByte(), outReader.ReadByte());
+                        DataContractJsonSerializer serializer = new(typeof(PolygonMovieMaker));
+                        serializer.WriteObject(inJson, testPmm);
+                        serializer.WriteObject(outJson, outPmm);
+                    }
+
+                        //Assert.AreEqual(reader.BaseStream.Length, outReader.BaseStream.Length, "入力ファイルと出力ファイルの長さが異なっている。");
+                        for (int i = 0; reader.BaseStream.Position < reader.BaseStream.Length; i++)
+                    {
+                        Assert.AreEqual(reader.ReadByte(), outReader.ReadByte(), $"{i} byte目で齟齬が発生");
                     }
                 }
             }
