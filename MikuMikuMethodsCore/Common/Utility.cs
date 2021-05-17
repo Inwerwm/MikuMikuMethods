237239560b3ec1dc229de88252a29cd90da0f262
Inwerwm <saclainvervm@gmail.com>
2021-05-17 22:58:18 +0900

    処理を改善

--- MikuMikuMethodsCore/Common/Utility.cs
+++ MikuMikuMethodsCore/Common/Utility.cs
@@ -17,27 +17,8 @@ namespace MikuMikuMethods
         /// <param name="map">もとの連番と新しい連番の変換マップリスト</param>
         /// <param name="query">変換対象リスト</param>
         /// <returns>変換されたリスト</returns>
-        public static List<T> OrderByMap<T>(List<int> map, List<T> query) where T : new()
-        {
-            List<T> r = new();
-
-            for (int i = 0; i < map.Count; i++)
-            {
-                if (map[i] < 0)
-                    continue;
-
-                int id = map[i];
-                while (r.Count <= id)
-                {
-                    r.Add(new T());
-                }
-
-                if (query.Count > i)
-                    r[id] = query[i];
-            }
-
-            return r;
-        }
+        public static List<T> OrderByMap<T>(List<int> map, List<T> query) =>
+            query.Zip(map, (t, i) => (Value: t, Index: i)).OrderBy(pair => pair.Index).Select(pair => pair.Value).ToList();
 
         /// <summary>
         /// 引数の中身を入れ替える
