bb84a13a95beb89a28cba34540bfb8f2972f5690
Inwerwm <saclainvervm@gmail.com>
2021-07-15 15:06:37 +0900

    PMXのヘッダ読み書きを実装

--- MikuMikuMethodsCore/PMX/PmxHeader.cs
+++ MikuMikuMethodsCore/PMX/PmxHeader.cs
@@ -1,9 +1,5 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MikuMikuMethods.PMX
 {
@@ -25,17 +21,17 @@ namespace MikuMikuMethods.PMX
         /// <summary>
         /// モデルの構成情報の要素数
         /// </summary>
-        private byte ConfigSize => 8;
+        private byte ConfigSize { get; set; } = 8;
 
         /// <summary>
         /// モデルの構成情報
         /// </summary>
-        public ModelConfig Config { get; } = new();
+        public PmxModelConfig Config { get; private set; }
 
         /// <summary>
         /// コンストラクタ
         /// </summary>
-        public PmxHeader(){}
+        public PmxHeader() { }
 
         /// <summary>
         /// データをバイナリから読み込む
@@ -43,7 +39,14 @@ namespace MikuMikuMethods.PMX
         /// <param name="reader">読み込み対象のリーダー</param>
         public void Read(BinaryReader reader)
         {
-            throw new NotImplementedException();
+            // "PMX "
+            reader.ReadBytes(4);
+            var version = reader.ReadSingle();
+            if (version < 2.0) throw new FormatException("PMXが非対応バージョンです。バージョン番号が未対応バージョンです。");
+
+            ConfigSize = reader.ReadByte();
+            if(ConfigSize != 8) throw new FormatException("PMXが非対応バージョンです。ヘッダデータが未対応の形式です。");
+            Config = new PmxModelConfig(reader.ReadBytes(ConfigSize));
         }
 
         /// <summary>
@@ -52,156 +55,11 @@ namespace MikuMikuMethods.PMX
         /// <param name="writer">書き込み対象のライター</param>
         public void Write(BinaryWriter writer)
         {
-            throw new NotImplementedException();
-        }
+            writer.Write(FormatName.ToCharArray(), 0, 4);
+            writer.Write(Version);
 
-        /// <summary>
-        /// モデルの各種情報
-        /// </summary>
-        public record ModelConfig
-        {
-            private static byte[] ZeroOrOne = { 0, 1 };
-            private static byte[] ZeroToFour = { 0, 1, 2, 3, 4 };
-            private static byte[] SizeNum = { 1, 2, 4 };
-
-            private byte encodingFormat;
-            private byte numOfAdditionalUV;
-            private byte sizeOfVertexIndex;
-            private byte sizeOfTextureIndex;
-            private byte sizeOfMaterialIndex;
-            private byte sizeOfBoneIndex;
-            private byte sizeOfMorphIndex;
-            private byte sizeOfBodyIndex;
-
-            /// <summary>
-            /// エンコード方式
-            ///     <list type="table">
-            ///         <item>
-            ///             <term>0</term>
-            ///             <description>UTF16</description>
-            ///         </item>
-            ///         <item>
-            ///             <term>1</term>
-            ///             <description>UTF8</description>
-            ///         </item>
-            ///     </list>
-            /// </summary>
-            public byte EncodingFormat
-            {
-                get => encodingFormat;
-                set
-                {
-                    if (ZeroOrOne.Contains(value))
-                        encodingFormat = value;
-                    else
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-            /// <summary>
-            /// <term>追加UV数</term>
-            /// <description>0 ～ 4</description>
-            /// </summary>
-            public byte NumOfAdditionalUV
-            {
-                get => numOfAdditionalUV;
-                set
-                {
-                    if (ZeroToFour.Contains(value))
-                        numOfAdditionalUV = value;
-                    else
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-            /// <summary>
-            /// <term>頂点Indexサイズ</term>
-            /// <description>1 | 2 | 4</description>
-            /// </summary>
-            public byte SizeOfVertexIndex
-            {
-                get => sizeOfVertexIndex;
-                set
-                {
-                    if (SizeNum.Contains(value))
-                        sizeOfVertexIndex = value;
-                    else
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-            /// <summary>
-            /// <term>テクスチャIndexサイズ</term>
-            /// <description>1 | 2 | 4</description>
-            /// </summary>
-            public byte SizeOfTextureIndex
-            {
-                get => sizeOfTextureIndex;
-                set
-                {
-                    if (SizeNum.Contains(value))
-                        sizeOfTextureIndex = value;
-                    else
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-            /// <summary>
-            /// <term>材質Indexサイズ</term>
-            /// <description>1 | 2 | 4</description>
-            /// </summary>
-            public byte SizeOfMaterialIndex
-            {
-                get => sizeOfMaterialIndex;
-                set
-                {
-                    if (SizeNum.Contains(value))
-                        sizeOfMaterialIndex = value;
-                    else
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-            /// <summary>
-            /// <term>ボーンIndexサイズ</term>
-            /// <description>1 | 2 | 4</description>
-            /// </summary>
-            public byte SizeOfBoneIndex
-            {
-                get => sizeOfBoneIndex;
-                set
-                {
-                    if (SizeNum.Contains(value))
-                        sizeOfBoneIndex = value;
-                    else
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-            /// <summary>
-            /// <term>モーフIndexサイズ</term>
-            /// <description>1 | 2 | 4</description>
-            /// </summary>
-            public byte SizeOfMorphIndex
-            {
-                get => sizeOfMorphIndex;
-                set
-                {
-                    if (SizeNum.Contains(value))
-                        sizeOfMorphIndex = value;
-                    else
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
-            /// <summary>
-            /// <term>剛体Indexサイズ</term>
-            /// <description>1 | 2 | 4</description>
-            /// </summary>
-            public byte SizeOfBodyIndex
-            {
-                get => sizeOfBodyIndex;
-                set
-                {
-                    if (SizeNum.Contains(value))
-                        sizeOfBodyIndex = value;
-                    else
-                        throw new ArgumentOutOfRangeException();
-                }
-            }
+            writer.Write(ConfigSize);
+            writer.Write(Config.ToArray());
         }
     }
 }
---
+++ MikuMikuMethodsCore/PMX/PmxModelConfig.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Linq;
+
+namespace MikuMikuMethods.PMX
+{
+    /// <summary>
+    /// モデルの各種情報
+    /// </summary>
+    public record PmxModelConfig
+    {
+        private static byte[] ZeroAndOne = { 0, 1 };
+        private static byte[] ZeroToFour = { 0, 1, 2, 3, 4 };
+        private static byte[] SizeNum = { 1, 2, 4 };
+
+        private byte encodingFormat;
+        private byte numOfAdditionalUV;
+        private byte sizeOfVertexIndex;
+        private byte sizeOfTextureIndex;
+        private byte sizeOfMaterialIndex;
+        private byte sizeOfBoneIndex;
+        private byte sizeOfMorphIndex;
+        private byte sizeOfBodyIndex;
+
+        /// <summary>
+        /// エンコード方式
+        ///     <list type="table">
+        ///         <item>
+        ///             <term>0</term>
+        ///             <description>UTF16</description>
+        ///         </item>
+        ///         <item>
+        ///             <term>1</term>
+        ///             <description>UTF8</description>
+        ///         </item>
+        ///     </list>
+        /// </summary>
+        public byte EncodingFormat
+        {
+            get => encodingFormat;
+            set
+            {
+                if (ZeroAndOne.Contains(value))
+                    encodingFormat = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>追加UV数</term>
+        /// <description>0 ～ 4</description>
+        /// </summary>
+        public byte NumOfAdditionalUV
+        {
+            get => numOfAdditionalUV;
+            set
+            {
+                if (ZeroToFour.Contains(value))
+                    numOfAdditionalUV = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>頂点Indexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfVertexIndex
+        {
+            get => sizeOfVertexIndex;
+            set
+            {
+                if (SizeNum.Contains(value))
+                    sizeOfVertexIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>テクスチャIndexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfTextureIndex
+        {
+            get => sizeOfTextureIndex;
+            set
+            {
+                if (SizeNum.Contains(value))
+                    sizeOfTextureIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>材質Indexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfMaterialIndex
+        {
+            get => sizeOfMaterialIndex;
+            set
+            {
+                if (SizeNum.Contains(value))
+                    sizeOfMaterialIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>ボーンIndexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfBoneIndex
+        {
+            get => sizeOfBoneIndex;
+            set
+            {
+                if (SizeNum.Contains(value))
+                    sizeOfBoneIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>モーフIndexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfMorphIndex
+        {
+            get => sizeOfMorphIndex;
+            set
+            {
+                if (SizeNum.Contains(value))
+                    sizeOfMorphIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>剛体Indexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfBodyIndex
+        {
+            get => sizeOfBodyIndex;
+            set
+            {
+                if (SizeNum.Contains(value))
+                    sizeOfBodyIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
+        public PmxModelConfig(byte[] config)
+        {
+            EncodingFormat = config[0];
+            NumOfAdditionalUV = config[1];
+            SizeOfVertexIndex = config[2];
+            SizeOfTextureIndex = config[3];
+            SizeOfMaterialIndex = config[4];
+            SizeOfBoneIndex = config[5];
+            SizeOfMorphIndex = config[6];
+            SizeOfBodyIndex = config[7];
+        }
+
+        public byte[] ToArray() => new[]
+        {
+            EncodingFormat,
+            NumOfAdditionalUV,
+            SizeOfVertexIndex,
+            SizeOfTextureIndex,
+            SizeOfMaterialIndex,
+            SizeOfBoneIndex,
+            SizeOfMorphIndex,
+            SizeOfBodyIndex
+        };
+    }
+}
