eb9defa92144fe1218a3fccdefe5aa1f6ce86b41
Inwerwm <saclainvervm@gmail.com>
2021-08-09 18:03:36 +0900

    バイナリからの読込関連メソッドを削除

--- MikuMikuMethodsCore/PMX/PmxModel.cs
+++ MikuMikuMethodsCore/PMX/PmxModel.cs
@@ -56,71 +56,10 @@ namespace MikuMikuMethods.PMX
         /// </summary>
         public List<PmxTexture> Textures { get; } = new();
 
-        private StringEncoder Encoder { get; set; }
-
         /// <summary>
         /// コンストラクタ
         /// </summary>
         public PmxModel() { }
-
-        /// <summary>
-        /// ファイル読込コンストラクタ
-        /// </summary>
-        /// <param name="filePath">Pmmファイルのパス</param>
-        public PmxModel(string filePath) : this()
-        {
-            Read(filePath);
-        }
-
-        /// <summary>
-        /// バイナリ読込コンストラクタ
-        /// </summary>
-        /// <param name="reader">PMMファイル</param>
-        public PmxModel(BinaryReader reader) : this()
-        {
-            Read(reader);
-        }
-
-        /// <summary>
-        /// データをファイルから読み込む
-        /// </summary>
-        /// <param name="filePath">読み込むファイルのパス</param>
-        public void Read(string filePath)
-        {
-            using (FileStream stream = new(filePath, FileMode.Open))
-            using (BinaryReader reader = new(stream, MikuMikuMethods.Encoding.ShiftJIS))
-            {
-                Read(reader);
-            }
-        }
-
-        /// <summary>
-        /// データをバイナリから読み込む
-        /// </summary>  
-        /// <param name="reader">読み込み対象のリーダー</param>
-        public void Read(BinaryReader reader)
-        {
-            Header.Read(reader);
-            Encoder = new(Header.EncodingOfModel);
-
-            ModelInfo.Read(reader, Encoder);
-            ReadFrames(reader, r => Vertices.Add(new(r)));
-            ReadFrames(reader, r => Faces.Add(new(r)));
-            ReadFrames(reader, r => Textures.Add(new(reader.ReadString()))); // 仮
-            ReadFrames(reader, r => Materials.Add(new(r)));
-            ReadFrames(reader, r => Bones.Add(new(r)));
-            ReadFrames(reader, r => Morphs.Add(new(r)));
-            ReadFrames(reader, r => Nodes.Add(new(r)));
-            ReadFrames(reader, r => Bodies.Add(new(r)));
-            ReadFrames(reader, r => Joints.Add(new(r)));
-        }
-
-        private void ReadFrames(BinaryReader reader, Action<BinaryReader> addElementToList)
-        {
-            var elementNum = reader.ReadUInt32();
-            for (int i = 0; i < elementNum; i++)
-                addElementToList(reader);
-        }
 
         /// <summary>
         /// データをファイルに書き込む
