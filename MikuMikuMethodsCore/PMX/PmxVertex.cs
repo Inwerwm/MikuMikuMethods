23b07070467af31947edc9464e867bb27f748e67
Inwerwm <saclainvervm@gmail.com>
2021-08-09 16:50:10 +0900

    頂点の読込メソッドの書ける分と書込メソッドを実装

--- MikuMikuMethodsCore/PMX/PmxVertex.cs
+++ MikuMikuMethodsCore/PMX/PmxVertex.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MikuMikuMethods.Extension;
+using System;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
@@ -31,7 +32,7 @@ namespace MikuMikuMethods.PMX
         /// <summary>
         /// 追加UVの座標
         /// </summary>
-        public Vector4[] AdditonalUVs { get; }
+        public Vector4[] AdditonalUVs { get; set; }
 
         /// <summary>
         /// ウェイト変形方式
@@ -50,7 +51,7 @@ namespace MikuMikuMethods.PMX
         /// <summary>
         /// SDEFパラメータ
         /// </summary>
-        public SDEFParams SDEF { get; set; }
+        public SDEFParams SDEF { get; set; } = new(new(), new(), new());
 
         /// <summary>
         /// エッジ倍率
@@ -61,16 +62,59 @@ namespace MikuMikuMethods.PMX
         /// バイナリ読込コンストラクタ
         /// </summary>
         /// <param name="reader">読み込み対象のリーダー</param>
-        public PmxVertex(BinaryReader reader)
+        public PmxVertex(BinaryReader reader, Indexer indexer, byte numOfAdditionalUV)
         {
-            Read(reader);
+            Read(reader, indexer, numOfAdditionalUV);
         }
 
         /// <summary>
         /// データをバイナリから読み込む
         /// </summary>
         /// <param name="reader">読み込み対象のリーダー</param>
-        public void Read(BinaryReader reader)
+        public void Read(BinaryReader reader, Indexer boneIndexer, byte numOfAdditionalUV)
+        {
+            Position = reader.ReadVector3();
+            Normal = reader.ReadVector3();
+            UV = reader.ReadVector2();
+
+            AdditonalUVs = numOfAdditionalUV <= 0 ? null : Enumerable.Range(0, numOfAdditionalUV).Select(_ => reader.ReadVector4()).ToArray();
+            WeightType = (PmxWeightType)reader.ReadByte();
+
+            switch (WeightType)
+            {
+                case PmxWeightType.BDEF1:
+                    ReadBDEF1Weights(reader, boneIndexer);
+                    break;
+                case PmxWeightType.BDEF2:
+                    ReadBDEF2Weights(reader, boneIndexer);
+                    break;
+                case PmxWeightType.BDEF4:
+                    ReadBDEF4Weights(reader, boneIndexer);
+                    break;
+                case PmxWeightType.SDEF:
+                    ReadSDEFWeights(reader, boneIndexer);
+                    break;
+            }
+
+            EdgeScale = reader.ReadSingle();
+        }
+
+        private void ReadBDEF1Weights(BinaryReader reader, Indexer indexer)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ReadBDEF2Weights(BinaryReader reader, Indexer indexer)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ReadBDEF4Weights(BinaryReader reader, Indexer indexer)
+        {
+            throw new NotImplementedException();
+        }
+
+        private void ReadSDEFWeights(BinaryReader reader, Indexer indexer)
         {
             throw new NotImplementedException();
         }
@@ -79,9 +123,72 @@ namespace MikuMikuMethods.PMX
         /// データをバイナリで書き込む
         /// </summary>
         /// <param name="writer">書き込み対象のライター</param>
-        public void Write(BinaryWriter writer)
+        public void Write(BinaryWriter writer, Indexer boneIndexer, IEnumerable<PmxBone> bones)
         {
-            throw new NotImplementedException();
+            writer.Write(Position);
+            writer.Write(Normal);
+            writer.Write(UV);
+
+            foreach (var item in AdditonalUVs)
+            {
+                writer.Write(item);
+            }
+
+            writer.Write((byte)WeightType);
+
+            var boneIdMap = bones.Select((Bone, Index) => (Bone, Index)).ToDictionary(b => b.Bone, b => b.Index);
+            switch (WeightType)
+            {
+                case PmxWeightType.BDEF1:
+                    WriteBDEF1Weights(writer, boneIndexer, boneIdMap);
+                    break;
+                case PmxWeightType.BDEF2:
+                    WriteBDEF2Weights(writer, boneIndexer, boneIdMap);
+                    break;
+                case PmxWeightType.BDEF4:
+                    WriteBDEF4Weights(writer, boneIndexer, boneIdMap);
+                    break;
+                case PmxWeightType.SDEF:
+                    WriteSDEFWeights(writer, boneIndexer, boneIdMap);
+                    break;
+            }
+
+            writer.Write(EdgeScale);
+        }
+
+        private void WriteBDEF1Weights(BinaryWriter writer, Indexer boneIndexer, Dictionary<PmxBone, int> boneIdMap)
+        {
+            boneIndexer.Write(writer, boneIdMap[Weights[0].Bone]);
+        }
+
+        private void WriteBDEF2Weights(BinaryWriter writer, Indexer boneIndexer, Dictionary<PmxBone, int> boneIdMap)
+        {
+            boneIndexer.Write(writer, boneIdMap[Weights[0].Bone]);
+            boneIndexer.Write(writer, boneIdMap[Weights[1].Bone]);
+            writer.Write(Weights[0].Value);
+        }
+
+        private void WriteBDEF4Weights(BinaryWriter writer, Indexer boneIndexer, Dictionary<PmxBone, int> boneIdMap)
+        {
+            boneIndexer.Write(writer, boneIdMap[Weights[0].Bone]);
+            boneIndexer.Write(writer, boneIdMap[Weights[1].Bone]);
+            boneIndexer.Write(writer, boneIdMap[Weights[2].Bone]);
+            boneIndexer.Write(writer, boneIdMap[Weights[3].Bone]);
+            writer.Write(Weights[0].Value);
+            writer.Write(Weights[1].Value);
+            writer.Write(Weights[2].Value);
+            writer.Write(Weights[3].Value);
+        }
+
+        private void WriteSDEFWeights(BinaryWriter writer, Indexer boneIndexer, Dictionary<PmxBone, int> boneIdMap)
+        {
+            boneIndexer.Write(writer, boneIdMap[Weights[0].Bone]);
+            boneIndexer.Write(writer, boneIdMap[Weights[1].Bone]);
+            writer.Write(Weights[0].Value);
+
+            writer.Write(SDEF.C);
+            writer.Write(SDEF.R0);
+            writer.Write(SDEF.R1);
         }
     }
 }
