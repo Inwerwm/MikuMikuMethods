01a5b385855600c4c96a375bb16a73e2989ada0a
Inwerwm <saclainvervm@gmail.com>
2021-08-09 17:09:27 +0900

    バイナリ入出力クラスを追加

---
+++ MikuMikuMethodsCore/PMX/PmxBinaryReader.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.PMX
+{
+    public class PmxBinaryReader
+    {
+        public static PmxModel ReadModel(string filePath)
+        {
+            var model = new PmxModel();
+
+            return model;
+        }
+    }
+}
---
+++ MikuMikuMethodsCore/PMX/PmxBinaryWriter.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.PMX
+{
+    public class PmxBinaryWriter
+    {
+        public void WriteModel(string filePath, PmxModel model)
+        {
+
+        }
+    }
+}
