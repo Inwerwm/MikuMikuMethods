0c8218612feb92ac46c41a8b724edee10d192ba9
Inwerwm <saclainvervm@gmail.com>
2021-08-09 17:22:36 +0900

    モデルクラスから読込メソッドの中身をコピーしReadFramesを作成

--- MikuMikuMethodsCore/PMX/PmxBinaryReader.cs
+++ MikuMikuMethodsCore/PMX/PmxBinaryReader.cs
@@ -16,10 +16,25 @@ namespace MikuMikuMethods.PMX
             {
                 var model = new PmxModel();
 
+                Header.Read(reader);
+                Encoder = new(Header.EncodingOfModel);
 
+                ModelInfo.Read(reader, Encoder);
+                ReadFrames(reader, r => Vertices.Add(new(r)));
+                ReadFrames(reader, r => Faces.Add(new(r)));
+                ReadFrames(reader, r => Textures.Add(new(reader.ReadString()))); // 仮
+                ReadFrames(reader, r => Materials.Add(new(r)));
+                ReadFrames(reader, r => Bones.Add(new(r)));
+                ReadFrames(reader, r => Morphs.Add(new(r)));
+                ReadFrames(reader, r => Nodes.Add(new(r)));
+                ReadFrames(reader, r => Bodies.Add(new(r)));
+                ReadFrames(reader, r => Joints.Add(new(r)));
 
                 return model;
             }
         }
+
+        private IEnumerable<T> ReadFrames<T>(BinaryReader reader, Func<BinaryReader, T> readData) where T : IPmxData =>
+            Enumerable.Range(0, reader.ReadInt32()).Select(_ => readData(reader)).ToArray();
     }
 }
