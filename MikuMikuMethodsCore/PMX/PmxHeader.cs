ad6df3186d08a9c998b77fc1ad5b868d74f5c392
Inwerwm <saclainvervm@gmail.com>
2021-08-09 17:16:17 +0900

    構成情報プロパティをモデルヘッダにインライン化

--- MikuMikuMethodsCore/PMX/PmxHeader.cs
+++ MikuMikuMethodsCore/PMX/PmxHeader.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.IO;
+using System.Linq;
 
 namespace MikuMikuMethods.PMX
 {
@@ -23,15 +24,154 @@ namespace MikuMikuMethods.PMX
         /// </summary>
         private byte ConfigSize { get; set; } = 8;
 
+        private static byte[] ZeroAndOne = { 0, 1 };
+        private static byte[] RangeOfAdditionalUV = { 0, 1, 2, 3, 4 };
+        private static byte[] IndexSize = { 1, 2, 4 };
+
+        private byte encodingFormat;
+        private byte numOfAdditionalUV;
+        private byte sizeOfVertexIndex;
+        private byte sizeOfTextureIndex;
+        private byte sizeOfMaterialIndex;
+        private byte sizeOfBoneIndex;
+        private byte sizeOfMorphIndex;
+        private byte sizeOfBodyIndex;
+
         /// <summary>
-        /// モデルの構成情報
+        /// エンコード方式
+        ///     <list type="table">
+        ///         <item>
+        ///             <term>0</term>
+        ///             <description>UTF16</description>
+        ///         </item>
+        ///         <item>
+        ///             <term>1</term>
+        ///             <description>UTF8</description>
+        ///         </item>
+        ///     </list>
         /// </summary>
-        public PmxModelConfig Config { get; private set; }
+        public byte EncodingFormat
+        {
+            get => encodingFormat;
+            set
+            {
+                if (ZeroAndOne.Contains(value))
+                    encodingFormat = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>追加UV数</term>
+        /// <description>0 ～ 4</description>
+        /// </summary>
+        public byte NumOfAdditionalUV
+        {
+            get => numOfAdditionalUV;
+            set
+            {
+                if (RangeOfAdditionalUV.Contains(value))
+                    numOfAdditionalUV = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>頂点Indexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfVertexIndex
+        {
+            get => sizeOfVertexIndex;
+            set
+            {
+                if (IndexSize.Contains(value))
+                    sizeOfVertexIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>テクスチャIndexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfTextureIndex
+        {
+            get => sizeOfTextureIndex;
+            set
+            {
+                if (IndexSize.Contains(value))
+                    sizeOfTextureIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>材質Indexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfMaterialIndex
+        {
+            get => sizeOfMaterialIndex;
+            set
+            {
+                if (IndexSize.Contains(value))
+                    sizeOfMaterialIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>ボーンIndexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfBoneIndex
+        {
+            get => sizeOfBoneIndex;
+            set
+            {
+                if (IndexSize.Contains(value))
+                    sizeOfBoneIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>モーフIndexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfMorphIndex
+        {
+            get => sizeOfMorphIndex;
+            set
+            {
+                if (IndexSize.Contains(value))
+                    sizeOfMorphIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        /// <summary>
+        /// <term>剛体Indexサイズ</term>
+        /// <description>1 | 2 | 4</description>
+        /// </summary>
+        public byte SizeOfBodyIndex
+        {
+            get => sizeOfBodyIndex;
+            set
+            {
+                if (IndexSize.Contains(value))
+                    sizeOfBodyIndex = value;
+                else
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+
 
         /// <summary>
         /// モデルの使用エンコード方式
         /// </summary>
-        public System.Text.Encoding EncodingOfModel => Config.EncodingFormat switch
+        public System.Text.Encoding EncodingOfModel => EncodingFormat switch
         {
             0 => System.Text.Encoding.Unicode,
             1 => System.Text.Encoding.UTF8,
@@ -55,7 +195,7 @@ namespace MikuMikuMethods.PMX
             if (version < 2.0) throw new FormatException("PMXが非対応バージョンです。バージョン番号が未対応バージョンです。");
 
             ConfigSize = reader.ReadByte();
-            if(ConfigSize != 8) throw new FormatException("PMXが非対応バージョンです。ヘッダデータが未対応の形式です。");
+            if (ConfigSize != 8) throw new FormatException("PMXが非対応バージョンです。ヘッダデータが未対応の形式です。");
             Config = new PmxModelConfig(reader.ReadBytes(ConfigSize));
         }
 
