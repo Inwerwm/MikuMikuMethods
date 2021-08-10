8b436193d595e298e1a39c97918642464b8f396f
Inwerwm <saclainvervm@gmail.com>
2021-08-10 15:49:46 +0900

    enumの仕様をPMXのバイナリに合わせた

--- MikuMikuMethodsCore/PMX/PmxMorph.cs
+++ MikuMikuMethodsCore/PMX/PmxMorph.cs
@@ -45,71 +45,75 @@ namespace MikuMikuMethods.PMX
         {
             Type = type;
         }
-    }
 
-    /// <summary>
-    /// モーフの表示パネル
-    /// </summary>
-    public enum MorphPanel
-    {
         /// <summary>
-        /// 眉(左下)
+        /// モーフの表示パネル
         /// </summary>
-        Brow,
-        /// <summary>
-        /// 目(左上)
-        /// </summary>
-        Eye,
-        /// <summary>
-        /// 口(右上)
-        /// </summary>
-        Lip,
-        /// <summary>
-        /// その他(右下)
-        /// </summary>
-        Other
-    }
+        public enum MorphPanel : byte
+        {
+            /// <summary>
+            /// システム予約
+            /// </summary>
+            System,
+            /// <summary>
+            /// 眉(左下)
+            /// </summary>
+            Brow,
+            /// <summary>
+            /// 目(左上)
+            /// </summary>
+            Eye,
+            /// <summary>
+            /// 口(右上)
+            /// </summary>
+            Lip,
+            /// <summary>
+            /// その他(右下)
+            /// </summary>
+            Other
+        }
 
-    /// <summary>
-    /// モーフ種類
-    /// </summary>
-    public enum MorphType
-    {
         /// <summary>
-        /// グループ
+        /// モーフ種類
         /// </summary>
-        Group,
-        /// <summary>
-        /// 頂点
-        /// </summary>
-        Vertex,
-        /// <summary>
-        /// ボーン
-        /// </summary>
-        Bone,
-        /// <summary>
-        /// UV
-        /// </summary>
-        UV,
-        /// <summary>
-        /// 追加UV1
-        /// </summary>
-        AdditionalUV1,
-        /// <summary>
-        /// 追加UV2
-        /// </summary>
-        AdditionalUV2,
-        /// <summary>
-        /// 追加UV3
-        /// </summary>
-        AdditionalUV3,
-        /// <summary>
-        /// 追加UV4
-        /// </summary>
-        AdditionalUV4,
-        /// <summary>
-        /// 材質
-        /// </summary>
-        Material
+        public enum MorphType : byte
+        {
+            /// <summary>
+            /// グループ
+            /// </summary>
+            Group,
+            /// <summary>
+            /// 頂点
+            /// </summary>
+            Vertex,
+            /// <summary>
+            /// ボーン
+            /// </summary>
+            Bone,
+            /// <summary>
+            /// UV
+            /// </summary>
+            UV,
+            /// <summary>
+            /// 追加UV1
+            /// </summary>
+            AdditionalUV1,
+            /// <summary>
+            /// 追加UV2
+            /// </summary>
+            AdditionalUV2,
+            /// <summary>
+            /// 追加UV3
+            /// </summary>
+            AdditionalUV3,
+            /// <summary>
+            /// 追加UV4
+            /// </summary>
+            AdditionalUV4,
+            /// <summary>
+            /// 材質
+            /// </summary>
+            Material
+        }
     }
 }
