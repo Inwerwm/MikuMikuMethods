a4eaead205ec1289cbff76318e8722a474f06147
Inwerwm <saclainvervm@gmail.com>
2021-08-12 18:31:12 +0900

    バージョンを最小化するメソッドを追加

--- MikuMikuMethodsCore/PMX/PmxModel.cs
+++ MikuMikuMethodsCore/PMX/PmxModel.cs
@@ -61,6 +61,19 @@ namespace MikuMikuMethods.PMX
         /// </summary>
         public List<PmxSoftBody> SoftBodies { get; } = new();
 
+        /// <summary>
+        /// このモデルのバージョン番号を最小化する
+        /// </summary>
+        public void ValidateVersion()
+        {
+            Header.Version = SoftBodies.Any() ||
+                             Materials.Any(m => m.EnableVertexColor || (m.Primitive == PmxMaterial.PrimitiveType.Point) || (m.Primitive == PmxMaterial.PrimitiveType.Line)) ||
+                             Joints.Any(j => j.Type != PmxJoint.JointType.SixDofWithSpring) ||
+                             Morphs.Any(m => (m.Type == PmxMorph.MorphType.Flip) || (m.Type == PmxMorph.MorphType.Impulse)) ||
+                             Vertices.Any(v => v.WeightType == PmxWeightType.QDEF)
+                           ? 2.1f : 2.0f;
+        }
+
         public override string ToString() => $"{Header} : {ModelInfo}";
     }
 }
