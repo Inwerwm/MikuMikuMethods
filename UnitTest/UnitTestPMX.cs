a38cd798d162d7c28ca37dd489b25f399fe110ce
Inwerwm <saclainvervm@gmail.com>
2021-08-13 20:59:01 +0900

    バイナリ単位の同一性テストを追加

--- UnitTest/UnitTestPMX.cs
+++ UnitTest/UnitTestPMX.cs
@@ -3,6 +3,7 @@ using MikuMikuMethods.PMX;
 using MikuMikuMethods.PMX.IO;
 using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Text.Json;
@@ -16,13 +17,31 @@ namespace UnitTest
         [TestMethod]
         public void PmxIOTest()
         {
-            PmxModel model = new("../../TestData/test.pmx");
-            model.Write("../../TestData/write.pmx");
-            PmxModel writed = new("../../TestData/write.pmx");
+            const string simpleI = "../../TestData/test.pmx";
+            const string simpleO = "../../TestData/write.pmx";
+            PmxModel model = new(simpleI);
+            model.Write(simpleO);
+            CompareBinary(simpleI, simpleO);
 
-            PmxModel txModel = new("../../TestData/tex.pmx");
-            txModel.Write("../../TestData/texW.pmx");
-            PmxModel texW = new("../../TestData/texW.pmx");
+            const string texI = "../../TestData/tex.pmx";
+            const string texO = "../../TestData/texW.pmx";
+            PmxModel txModel = new(texI);
+            txModel.Write(texO);
+            CompareBinary(texI, texO);
+        }
+
+        private void CompareBinary(string filePath1, string filePath2)
+        {
+            var b1 = File.ReadAllBytes(filePath1);
+            var b2 = File.ReadAllBytes(filePath2);
+
+            if (b1.Length != b2.Length)
+                Assert.Fail("バイナリデータの長さが違います。");
+
+            foreach (var b in b1.Zip(b2))
+            {
+                Assert.AreEqual(b.First, b.Second);
+            }
         }
 
         [TestMethod]
