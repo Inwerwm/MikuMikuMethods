dce384a133d3c11c869baaf00c535db941d517a5
Inwerwm <saclainvervm@gmail.com>
2021-08-14 15:51:37 +0900

    Emdファイルの内部表現を作成

--- MikuMikuMethodsCore/MME/EmdData.cs
+++ MikuMikuMethodsCore/MME/EmdData.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using MikuMikuMethods.MME.Element;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,30 @@ using System.Threading.Tasks;
 
 namespace MikuMikuMethods.MME
 {
-    class EmdData
+    /// <summary>
+    /// Emdファイルの内部表現
+    /// </summary>
+    public class EmdData
     {
+        /// <summary>
+        /// EMMのバージョン
+        /// </summary>
+        public int Version { get; private set; }
+
+        /// <summary>
+        /// オブジェクトに適用するエフェクトの情報
+        /// </summary>
+        public EmmMaterial Material { get; init; }
+
+        /// <summary>
+        /// サブセット展開されたときの各材質に適用するエフェクトの情報
+        /// </summary>
+        public List<EmmMaterial> Subsets { get; init; }
+
+        public EmdData(EmmMaterial material)
+        {
+            Material = material;
+            Subsets = new();
+        }
     }
 }
