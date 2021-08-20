930e0753737f948eeae971e142166c1b411a3acc
Inwerwm <saclainvervm@gmail.com>
2021-08-20 22:47:42 +0900

    pmmの調査プログラム

--- TestWithConsole/Program.cs
+++ TestWithConsole/Program.cs
@@ -9,9 +9,23 @@ namespace TestWithConsole
         const string dataDir = "../../Data/";
 
         private static string Path(string filename) => dataDir + filename;
+        private static void WL(string? value = "") => Console.WriteLine(value);
+        private static void WLL(string? value = "") => Console.WriteLine(value + Environment.NewLine);
 
         static void Main(string[] args)
         {
+            var pmm = new MikuMikuMethods.PMM.PolygonMovieMaker(Path("pmm.pmm"));
+            WLL(pmm.Models.Print("\n"));
+
+            var miku = pmm.Models[0];
+            var haku = pmm.Models[1];
+            WLL(pmm.Models.Select(m => (m.RenderConfig.RenderOrder, m.Name)).Print("\n"));
+
+            miku.RenderConfig.RenderOrder = 2;
+            haku.RenderConfig.RenderOrder = 1;
+            WLL(pmm.Models.Select(m => (m.RenderConfig.RenderOrder, m.Name)).Print("\n"));
+
+            pmm.Write(Path("out.pmm"));
         }
     }
 
