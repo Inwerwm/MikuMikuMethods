f52d84fa1fa2acc8397fa1d76cd67176c805dab6
Inwerwm <saclainvervm@gmail.com>
2021-08-20 22:15:08 +0900

    コンソールプログラムでいろいろ試したいとき用プロジェクトを作成

---
+++ TestWithConsole/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace TestWithConsole
+{
+    class Program
+    {
+        const string dataDir = "../../Data/";
+
+        private static string Path(string filename) => dataDir + filename;
+
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    static class Extension
+    {
+        public static string Print<T>(this IEnumerable<T> collection) => Print(collection, ", ");
+        public static string Print<T>(this IEnumerable<T> collection, string delimiter) => collection.Select(elm => elm.ToString()).Aggregate((acm, elm) => $"{acm}{delimiter}{elm}");
+    }
+}
