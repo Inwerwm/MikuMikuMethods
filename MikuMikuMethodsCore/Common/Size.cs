bb36a34fab4dce829c83f3d3f6ab2e60c5520633
Inwerwm <saclainvervm@gmail.com>
2021-08-22 02:30:55 +0900

    サイズ格納レコードを追加

---
+++ MikuMikuMethodsCore/Common/Size.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.Common
+{
+    public record Size(int Width, int Height);
+}
