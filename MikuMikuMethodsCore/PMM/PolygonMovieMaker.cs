4b808ff0c01ef7b1ee2f8d93cf55e87862796192
Inwerwm <saclainvervm@gmail.com>
2021-08-22 02:54:13 +0900

    ドキュメントコメントを追加

--- MikuMikuMethodsCore/PMM/PolygonMovieMaker.cs
+++ MikuMikuMethodsCore/PMM/PolygonMovieMaker.cs
@@ -7,22 +7,61 @@ using System.Threading.Tasks;
 
 namespace MikuMikuMethods.PMM
 {
+    /// <summary>
+    /// MMDプロジェクトファイル
+    /// </summary>
     public class PolygonMovieMaker
     {
+        /// <summary>
+        /// PMMファイルバージョン
+        /// </summary>
         public string Version { get; internal set; }
+        /// <summary>
+        /// 出力解像度
+        /// </summary>
         public Size OutputResolution { get; set; }
 
+        /// <summary>
+        /// カメラ
+        /// </summary>
         public PmmCamera Camera { get; }
+        /// <summary>
+        /// 照明
+        /// </summary>
         public PmmLight Light { get; }
+        /// <summary>
+        /// セルフ影
+        /// </summary>
         public PmmSelfShadow SelfShadow { get; }
+        /// <summary>
+        /// 物理
+        /// </summary>
         public PmmPhysics Physics { get; }
+        /// <summary>
+        /// アクセサリー
+        /// </summary>
         public List<PmmAccessory> Accessories { get; }
+        /// <summary>
+        /// モデル
+        /// </summary>
         public List<PmmModel> Models { get; }
         
+        /// <summary>
+        /// 背景と音声
+        /// </summary>
         public PmmMedia Media { get; }
 
+        /// <summary>
+        /// 3Dビュー画面の状態
+        /// </summary>
         public PmmRenderPane RenderPane { get; }
+        /// <summary>
+        /// キーフレーム編集画面の状態
+        /// </summary>
         public PmmKeyFramePane KeyFramePane { get; }
+        /// <summary>
+        /// 各種操作パネル画面の状態
+        /// </summary>
         public PmmPanelPane PanelPane { get; }
     }
 }
