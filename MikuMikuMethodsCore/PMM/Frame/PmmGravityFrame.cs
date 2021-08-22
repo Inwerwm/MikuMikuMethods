220659ced8431a7b7e13733f293f90fae9383452
Inwerwm <saclainvervm@gmail.com>
2021-08-22 11:52:55 +0900

    物理と重力フレームを実装

---
+++ MikuMikuMethodsCore/PMM/Frame/PmmGravityFrame.cs
@@ -0,0 +1,27 @@
+﻿using System.Numerics;
+
+namespace MikuMikuMethods.PMM.Frame
+{
+    public class PmmGravityFrame : IPmmFrame
+    {
+        public int Position { get; set; } = 0;
+        public bool IsSelected { get; set; } = false;
+
+        /// <summary>
+        /// ノイズ量
+        /// nullならなし
+        /// </summary>
+        public int? Noize { get; set; } = null;
+        /// <summary>
+        /// 現在の加速度
+        /// </summary>
+        public float Acceleration { get; set; } = 9.8f;
+        /// <summary>
+        /// 現在の方向
+        /// </summary>
+        public Vector3 Direction { get; set; } = new(0, -1, 0);
+
+        public bool Equals(IPmmFrame other) =>
+            other is PmmGravityFrame f && Position == f.Position;
+    }
+}
--- MikuMikuMethodsCore/PMM/PmmPhysics.cs
+++ MikuMikuMethodsCore/PMM/PmmPhysics.cs
@@ -1,6 +1,59 @@
-﻿namespace MikuMikuMethods.PMM
+﻿using MikuMikuMethods.PMM.Frame;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace MikuMikuMethods.PMM
 {
     public class PmmPhysics
     {
+        /// <summary>
+        /// ノイズ量
+        /// nullならなし
+        /// </summary>
+        public int? GravityNoize { get; set; } = null;
+        /// <summary>
+        /// 現在の加速度
+        /// </summary>
+        public float GravityAcceleration { get; set; } = 9.8f;
+        /// <summary>
+        /// 現在の方向
+        /// </summary>
+        public Vector3 GravityDirection { get; set; } = new(0, -1, 0);
+
+        /// <summary>
+        /// 重力フレーム
+        /// </summary>
+        public List<PmmGravityFrame> Frames { get; } = new();
+
+        /// <summary>
+        /// 物理演算モード
+        /// </summary>
+        public enum PhysicsMode : byte
+        {
+            /// <summary>
+            /// 演算しない
+            /// </summary>
+            Disable,
+            /// <summary>
+            /// 常に演算
+            /// </summary>
+            Always,
+            /// <summary>
+            /// オン/オフモード
+            /// </summary>
+            Switchable,
+            /// <summary>
+            /// トレースモード
+            /// </summary>
+            Trace
+        }
+        /// <summary>
+        /// 物理演算モード
+        /// </summary>
+        public PhysicsMode CurrentPhysicsMode { get; set; } = PhysicsMode.Always;
+        /// <summary>
+        /// 物理床のOn/Off
+        /// </summary>
+        public bool EnableGroundPhysics { get; set; } = true;
     }
 }
