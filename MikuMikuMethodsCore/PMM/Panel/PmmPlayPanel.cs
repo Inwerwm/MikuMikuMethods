c296fec138d606b60b11ca7f18d47649b0cb1237
Inwerwm <saclainvervm@gmail.com>
2021-08-22 17:57:43 +0900

    パネル画面クラスを実装

---
+++ MikuMikuMethodsCore/PMM/Panel/PmmPlayPanel.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.PMM.Panel
+{
+    public class PmmPlayPanel
+    {
+        /// <summary>
+        /// 繰り返し再生を行うか
+        /// </summary>
+        public bool EnableRepeat { get; set; } = false;
+        /// <summary>
+        /// フレ・ストップ
+        /// </summary>
+        public bool EnableMoveCurrentFrameToPlayStopping { get; set; } = false;
+        /// <summary>
+        /// フレ・スタート
+        /// </summary>
+        public bool EnableStartFromCurrentFrame { get; set; } = false;
+
+        /// <summary>
+        /// 再生開始フレーム
+        /// </summary>
+        public int PlayStartFrame { get; set; } = 0;
+        /// <summary>
+        /// 再生停止フレーム
+        /// </summary>
+        public int PlayStopFrame { get; set; } = 0;
+
+        /// <summary>
+        /// 視点追従対象
+        /// </summary>
+        public TrackingTarget CameraTrackingTarget { get; set; } = TrackingTarget.None;
+        /// <summary>
+        /// 視点追従対象
+        /// </summary>
+        public enum TrackingTarget : byte
+        {
+            /// <summary>
+            /// なし
+            /// </summary>
+            None,
+            /// <summary>
+            /// モデル
+            /// </summary>
+            Model,
+            /// <summary>
+            /// ボーン
+            /// </summary>
+            Bone,
+        }
+    }
+}
--- MikuMikuMethodsCore/PMM/PmmPanelPane.cs
+++ MikuMikuMethodsCore/PMM/PmmPanelPane.cs
@@ -1,6 +1,73 @@
-﻿namespace MikuMikuMethods.PMM
+﻿using MikuMikuMethods.PMM.Panel;
+
+namespace MikuMikuMethods.PMM
 {
     public class PmmPanelPane
     {
+        /// <summary>
+        /// カメラ操作パネル開閉状態
+        /// </summary>
+        public bool DoesOpenCameraPanel { get; set; } = true;
+        /// <summary>
+        /// ライト操作パネル開閉状態
+        /// </summary>
+        public bool DoesOpenLightPanel { get; set; } = true;
+        /// <summary>
+        /// アクセサリ操作パネル開閉状態
+        /// </summary>
+        public bool DoesOpenAccessaryPanel { get; set; } = true;
+        /// <summary>
+        /// ボーン操作パネル開閉状態
+        /// </summary>
+        public bool DoesOpenBonePanel { get; set; } = true;
+        /// <summary>
+        /// 表情操作パネル開閉状態
+        /// </summary>
+        public bool DoesOpenMorphPanel { get; set; } = true;
+        /// <summary>
+        /// セルフ影操作パネル開閉状態
+        /// </summary>
+        public bool DoesOpenSelfShadowPanel { get; set; } = true;
+
+        /// <summary>
+        /// 選択中のモデル
+        /// </summary>
+        public PmmModel SelectedModel { get; set; }
+        /// <summary>
+        /// 選択中のアクセサリ
+        /// </summary>
+        public PmmAccessory SelectedAccessory { get; set; }
+        /// <summary>
+        /// 選択中のボーン操作方法
+        /// </summary>
+        public BoneOperation SelectedBoneOperation { get; set; }
+        /// <summary>
+        /// MMD上でのボーン操作
+        /// </summary>
+        public enum BoneOperation : int
+        {
+            /// <summary>
+            /// 選択
+            /// </summary>
+            Select,
+            /// <summary>
+            /// 矩形選択
+            /// </summary>
+            RectangleSelect,
+            /// <summary>
+            /// 移動
+            /// </summary>
+            Move,
+            /// <summary>
+            /// 回転
+            /// </summary>
+            Rotate,
+            /// <summary>
+            /// 無選択
+            /// </summary>
+            None
+        }
+
+        public PmmPlayPanel PlayPanel { get; } = new();
     }
 }
