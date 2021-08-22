e94cd69303a2972aba743d48abd06bb6e89c8da2
Inwerwm <saclainvervm@gmail.com>
2021-08-22 11:49:32 +0900

    フレームインターフェイスに IEquatable を継承させた

--- MikuMikuMethodsCore/PMM/Frame/IPmmFrame.cs
+++ MikuMikuMethodsCore/PMM/Frame/IPmmFrame.cs
@@ -1,20 +1,19 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace MikuMikuMethods.PMM.Frame
 {
-    public interface IPmmFrame
+    public interface IPmmFrame : IEquatable<IPmmFrame>
     {
         /// <summary>
         /// フレーム位置
         /// </summary>
-        public int Position { get; set; }
+        int Position { get; set; }
         /// <summary>
         /// 選択状態か
         /// </summary>
-        public bool IsSelected { get; set; }
+        bool IsSelected { get; set; }
+
+        // Equals では List 内で重複していたら困る情報を比較するよう実装する
+        // それにより Distinct を実行するだけで重複を除去できるようにする
     }
 }
