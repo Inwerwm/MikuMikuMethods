6c902ae85d688fb1685f06f0be047e3918524afb
Inwerwm <saclainvervm@gmail.com>
2021-08-22 11:15:12 +0900

    プロパティの初期値を追加

--- MikuMikuMethodsCore/PMM/PolygonMovieMaker.cs
+++ MikuMikuMethodsCore/PMM/PolygonMovieMaker.cs
@@ -11,53 +11,53 @@ namespace MikuMikuMethods.PMM
         /// <summary>
         /// PMMファイルバージョン
         /// </summary>
-        public string Version { get; internal set; }
+        public string Version { get; internal set; } = "Polygon Movie maker 0002";
         /// <summary>
         /// 出力解像度
         /// </summary>
-        public Size OutputResolution { get; set; }
+        public Size OutputResolution { get; set; } = new(1920, 1080);
 
         /// <summary>
         /// カメラ
         /// </summary>
-        public PmmCamera Camera { get; }
+        public PmmCamera Camera { get; } = new();
         /// <summary>
         /// 照明
         /// </summary>
-        public PmmLight Light { get; }
+        public PmmLight Light { get; } = new();
         /// <summary>
         /// セルフ影
         /// </summary>
-        public PmmSelfShadow SelfShadow { get; }
+        public PmmSelfShadow SelfShadow { get; } = new();
         /// <summary>
         /// 物理
         /// </summary>
-        public PmmPhysics Physics { get; }
+        public PmmPhysics Physics { get; } = new();
         /// <summary>
         /// アクセサリー
         /// </summary>
-        public List<PmmAccessory> Accessories { get; }
+        public List<PmmAccessory> Accessories { get; } = new();
         /// <summary>
         /// モデル
         /// </summary>
-        public List<PmmModel> Models { get; }
+        public List<PmmModel> Models { get; } = new();
 
         /// <summary>
         /// 背景と音声
         /// </summary>
-        public PmmMedia Media { get; }
+        public PmmMedia Media { get; } = new();
 
         /// <summary>
         /// 3Dビュー画面の状態
         /// </summary>
-        public PmmRenderPane RenderPane { get; }
+        public PmmRenderPane RenderPane { get; } = new();
         /// <summary>
         /// キーフレーム編集画面の状態
         /// </summary>
-        public PmmKeyFramePane KeyFramePane { get; }
+        public PmmKeyFramePane KeyFramePane { get; } = new();
         /// <summary>
         /// 各種操作パネル画面の状態
         /// </summary>
-        public PmmPanelPane PanelPane { get; }
+        public PmmPanelPane PanelPane { get; } = new();
     }
 }
