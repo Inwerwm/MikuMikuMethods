1015d0148bb95708b3d7c7c8d82302375fd0f7c5
Inwerwm <saclainvervm@gmail.com>
2021-08-26 16:39:32 +0900

    セルフ影クラスを実装

---
+++ MikuMikuMethodsCore/PMM/Frame/PmmSelfShadowFrame.cs
@@ -0,0 +1,38 @@
+﻿namespace MikuMikuMethods.PMM.Frame
+{
+    public class PmmSelfShadowFrame : IPmmFrame
+    {
+        public int Frame { get; set; }
+        public bool IsSelected { get; set; }
+
+        /// <summary>
+        /// 影モード
+        /// </summary>
+        public Shadow ShadowMode { get; set; }
+        /// <summary>
+        /// 影範囲
+        /// </summary>
+        public float ShadowRange { get; set; }
+
+        /// <summary>
+        /// 影のモード
+        /// </summary>
+        public enum Shadow : byte
+        {
+            /// <summary>
+            /// 影なし
+            /// </summary>
+            None,
+            /// <summary>
+            /// モード1
+            /// </summary>
+            Mode1,
+            /// <summary>
+            /// モード2
+            /// </summary>
+            Mode2,
+        }
+
+        public bool Equals(IPmmFrame other) => other is PmmSelfShadowFrame f && f.Frame == Frame;
+    }
+}
--- MikuMikuMethodsCore/PMM/PmmSelfShadow.cs
+++ MikuMikuMethodsCore/PMM/PmmSelfShadow.cs
@@ -1,6 +1,19 @@
-﻿namespace MikuMikuMethods.PMM
+﻿using MikuMikuMethods.PMM.Frame;
+using System.Collections.Generic;
+
+namespace MikuMikuMethods.PMM
 {
     public class PmmSelfShadow
     {
+        /// <summary>
+        /// セルフ影のOn/Off
+        /// </summary>
+        public bool EnableSelfShadow { get; set; } = true;
+        /// <summary>
+        /// 影範囲
+        /// </summary>
+        public float ShadowRange { get; set; } = 8875;
+
+        public List<PmmSelfShadowFrame> Frames { get; } = new();
     }
 }
