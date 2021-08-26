53d120c07432eb4f881d916dddf0cbfa77d2fed0
Inwerwm <saclainvervm@gmail.com>
2021-08-26 16:28:53 +0900

    ライトクラスを実装

--- MikuMikuMethodsCore/PMM/PmmLight.cs
+++ MikuMikuMethodsCore/PMM/PmmLight.cs
@@ -1,6 +1,25 @@
-﻿namespace MikuMikuMethods.PMM
+﻿using MikuMikuMethods.Extension;
+using MikuMikuMethods.PMM.Frame;
+using System.Collections.Generic;
+using System.Numerics;
+
+namespace MikuMikuMethods.PMM
 {
     public class PmmLight
     {
+        public List<PmmLightFrame> Frames { get; } = new();
+        public TemporaryLightState Current { get; } = new();
+
+        public class TemporaryLightState
+        {
+            /// <summary>
+            /// 色(RGBのみ使用)
+            /// </summary>
+            public ColorF Color { get; set; } = System.Drawing.Color.FromArgb(154, 154, 154).ToColorF();
+            /// <summary>
+            /// 位置
+            /// </summary>
+            public Vector3 Position { get; set; } = new(-0.5f, -1.0f, 0.5f);
+        }
     }
 }
