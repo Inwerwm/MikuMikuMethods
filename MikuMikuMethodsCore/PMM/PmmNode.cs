4c1af26e6cb4874c32c6db24aa068591f55c4fd2
Inwerwm <saclainvervm@gmail.com>
2021-10-29 22:28:42 +0900

    pmmファイルの読み込み時に確定できなかったのでプロパティ値のコンストラクタでの要求をやめた

--- MikuMikuMethodsCore/PMM/PmmNode.cs
+++ MikuMikuMethodsCore/PMM/PmmNode.cs
@@ -4,15 +4,8 @@ namespace MikuMikuMethods.PMM
 {
     public record PmmNode
     {
-        public string Name { get; init; }
+        public string Name { get; set; }
         public bool doesOpen { get; set; } = false;
-        public ImmutableArray<IPmmModelElement> Elements { get; init; }
-
-        public PmmNode(string name, bool doesOpen, ImmutableArray<IPmmModelElement> elements)
-        {
-            Name = name;
-            this.doesOpen = doesOpen;
-            Elements = elements;
-        }
+        public ImmutableArray<IPmmModelElement> Elements { get; init; } = new();
     }
 }
