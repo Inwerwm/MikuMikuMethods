ac094824f7d7e75b3838080ba58c3f3e656c3721
Inwerwm <saclainvervm@gmail.com>
2021-10-29 21:04:33 +0900

    Pmmの入出力クラスを追加

---
+++ MikuMikuMethodsCore/PMM/IO/PmmFileReader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.PMM.IO
+{
+    internal static class PmmFileReader
+    {
+    }
+}
