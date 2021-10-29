ccec8ebe1690316f620ac18d98b43483700a3bf0
Inwerwm <saclainvervm@gmail.com>
2021-10-29 21:26:06 +0900

    キーフレーム画面クラスにふさわしいと思しきプロパティを追加・移動

--- MikuMikuMethodsCore/PMM/PmmKeyFramePane.cs
+++ MikuMikuMethodsCore/PMM/PmmKeyFramePane.cs
@@ -2,5 +2,51 @@
 {
     public class PmmKeyFramePane
     {
+        /// <summary>
+        /// 編集画面の表示幅
+        /// </summary>
+        public int Width { get; set; }
+        /// <summary>
+        /// 現在カメラ編集モードか？
+        /// </summary>
+        public bool IsCameraMode { get; set; }
+        /// <summary>
+        /// 選択中のモデル
+        /// </summary>
+        public PmmModel SelectedModel { get; set; }
+        /// <summary>
+        /// 選択中のアクセサリ
+        /// </summary>
+        public PmmAccessory SelectedAccessory { get; set; }
+        /// <summary>
+        /// 選択中のボーン操作方法
+        /// </summary>
+        public BoneOperation SelectedBoneOperation { get; set; }
+        /// <summary>
+        /// MMD上でのボーン操作
+        /// </summary>
+        public enum BoneOperation : int
+        {
+            /// <summary>
+            /// 選択
+            /// </summary>
+            Select,
+            /// <summary>
+            /// 矩形選択
+            /// </summary>
+            RectangleSelect,
+            /// <summary>
+            /// 移動
+            /// </summary>
+            Move,
+            /// <summary>
+            /// 回転
+            /// </summary>
+            Rotate,
+            /// <summary>
+            /// 無選択
+            /// </summary>
+            None
+        }
     }
 }
--- MikuMikuMethodsCore/PMM/PmmPanelPane.cs
+++ MikuMikuMethodsCore/PMM/PmmPanelPane.cs
@@ -29,45 +29,6 @@ namespace MikuMikuMethods.PMM
         /// </summary>
         public bool DoesOpenSelfShadowPanel { get; set; } = true;
 
-        /// <summary>
-        /// 選択中のモデル
-        /// </summary>
-        public PmmModel SelectedModel { get; set; }
-        /// <summary>
-        /// 選択中のアクセサリ
-        /// </summary>
-        public PmmAccessory SelectedAccessory { get; set; }
-        /// <summary>
-        /// 選択中のボーン操作方法
-        /// </summary>
-        public BoneOperation SelectedBoneOperation { get; set; }
-        /// <summary>
-        /// MMD上でのボーン操作
-        /// </summary>
-        public enum BoneOperation : int
-        {
-            /// <summary>
-            /// 選択
-            /// </summary>
-            Select,
-            /// <summary>
-            /// 矩形選択
-            /// </summary>
-            RectangleSelect,
-            /// <summary>
-            /// 移動
-            /// </summary>
-            Move,
-            /// <summary>
-            /// 回転
-            /// </summary>
-            Rotate,
-            /// <summary>
-            /// 無選択
-            /// </summary>
-            None
-        }
-
         public PmmPlayPanel PlayPanel { get; } = new();
     }
 }
