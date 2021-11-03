13d0469211f653c9cd3d03b4b1afa1a91da924c3
Inwerwm <saclainvervm@gmail.com>
2021-11-03 18:25:54 +0900

    PMMのテストクラスと入出力テストメソッドを作成

---
+++ UnitTest/UnitTestPmm.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UnitTest
+{
+    [Microsoft.VisualStudio.TestTools.UnitTesting.TestClass]
+    public class UnitTestPmm
+    {
+        [TestMethod]
+        public void IOTest()
+        {
+
+        }
+    }
+}
