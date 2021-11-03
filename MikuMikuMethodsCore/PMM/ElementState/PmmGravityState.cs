153cf68b8c66921baa08b7c96cf6437341cb61a8
Inwerwm <saclainvervm@gmail.com>
2021-11-03 14:24:20 +0900

    重力フレームから重力情報クラスを抽出

---
+++ MikuMikuMethodsCore/PMM/ElementState/PmmGravityState.cs
@@ -0,0 +1,22 @@
+﻿using System.Numerics;
+
+namespace MikuMikuMethods.PMM.ElementState
+{
+    public class PmmGravityState
+    {
+
+        /// <summary>
+        /// ノイズ量
+        /// nullならなし
+        /// </summary>
+        public int? Noize { get; set; } = null;
+        /// <summary>
+        /// 現在の加速度
+        /// </summary>
+        public float Acceleration { get; set; } = 9.8f;
+        /// <summary>
+        /// 現在の方向
+        /// </summary>
+        public Vector3 Direction { get; set; } = new(0, -1, 0);
+    }
+}
