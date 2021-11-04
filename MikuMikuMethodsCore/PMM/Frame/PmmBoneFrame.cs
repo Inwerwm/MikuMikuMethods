b9df0514e22f601d5faa9c8f153c4e96af519a48
Inwerwm <saclainvervm@gmail.com>
2021-11-04 22:58:07 +0900

    補間曲線辞書にインスタンスが入っていなかったのを修正

--- MikuMikuMethodsCore/PMM/Frame/PmmBoneFrame.cs
+++ MikuMikuMethodsCore/PMM/Frame/PmmBoneFrame.cs
@@ -1,4 +1,5 @@
 ﻿using MikuMikuMethods.PMM.ElementState;
+using System.Collections.Generic;
 using System.Collections.ObjectModel;
 
 namespace MikuMikuMethods.PMM.Frame
@@ -11,6 +12,12 @@ namespace MikuMikuMethods.PMM.Frame
         /// <summary>
         /// 補間曲線
         /// </summary>
-        public ReadOnlyDictionary<InterpolationItem, InterpolationCurve> InterpolationCurces { get; init; }
+        public ReadOnlyDictionary<InterpolationItem, InterpolationCurve> InterpolationCurces { get; } = new(new Dictionary<InterpolationItem, InterpolationCurve>()
+        {
+            { InterpolationItem.XPosition, new() },
+            { InterpolationItem.YPosition, new() },
+            { InterpolationItem.ZPosition, new() },
+            { InterpolationItem.Rotation, new() },
+        });
     }
 }
