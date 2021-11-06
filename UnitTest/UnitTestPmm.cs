344f54330763d1ad71dcf6fb205cdb46319d855c
Inwerwm <saclainvervm@gmail.com>
2021-11-06 16:37:44 +0900

    存在しない順序値をGetされたときは例外を投げるのではなくnullを返すことを期待するように変更 値比較で型違いで不具合が起きないようにした

--- UnitTest/UnitTestPmm.cs
+++ UnitTest/UnitTestPmm.cs
@@ -36,40 +36,40 @@ namespace UnitTest
             pmm1.Models.Add(model2);
 
             // 追加した順に設定される
-            Assert.AreEqual(0, pmm1.GetRenderOrder(model1));
-            Assert.AreEqual(1, pmm1.GetRenderOrder(model2));
-            Assert.AreEqual(0, pmm1.GetCalculateOrder(model1));
-            Assert.AreEqual(1, pmm1.GetCalculateOrder(model2));
+            Assert.AreEqual((byte)0, pmm1.GetRenderOrder(model1).Value);
+            Assert.AreEqual((byte)1, pmm1.GetRenderOrder(model2).Value);
+            Assert.AreEqual((byte)0, pmm1.GetCalculateOrder(model1).Value);
+            Assert.AreEqual((byte)1, pmm1.GetCalculateOrder(model2).Value);
 
             // 同じモデルが違う PMM に追加されても変わらない
             // 初期実装だとこれがダメだった
             pmm2.Models.Add(model2);
             pmm2.Models.Add(model1);
 
-            Assert.AreEqual(0, pmm2.GetRenderOrder(model2));
-            Assert.AreEqual(1, pmm2.GetRenderOrder(model1));
-            Assert.AreEqual(0, pmm2.GetCalculateOrder(model2));
-            Assert.AreEqual(1, pmm2.GetCalculateOrder(model1));
+            Assert.AreEqual((byte)0, pmm2.GetRenderOrder(model2).Value);
+            Assert.AreEqual((byte)1, pmm2.GetRenderOrder(model1).Value);
+            Assert.AreEqual((byte)0, pmm2.GetCalculateOrder(model2).Value);
+            Assert.AreEqual((byte)1, pmm2.GetCalculateOrder(model1).Value);
 
-            Assert.AreEqual(0, pmm1.GetRenderOrder(model1));
-            Assert.AreEqual(1, pmm1.GetRenderOrder(model2));
-            Assert.AreEqual(0, pmm1.GetCalculateOrder(model1));
-            Assert.AreEqual(1, pmm1.GetCalculateOrder(model2));
+            Assert.AreEqual((byte)0, pmm1.GetRenderOrder(model1).Value);
+            Assert.AreEqual((byte)1, pmm1.GetRenderOrder(model2).Value);
+            Assert.AreEqual((byte)0, pmm1.GetCalculateOrder(model1).Value);
+            Assert.AreEqual((byte)1, pmm1.GetCalculateOrder(model2).Value);
 
             // 順序設定の反映がされているか
             pmm1.SetRenderOrder(model1, 1);
             pmm1.SetCalculateOrder(model2, 0);
 
-            Assert.AreEqual(0, pmm1.GetRenderOrder(model2));
-            Assert.AreEqual(1, pmm1.GetRenderOrder(model1));
-            Assert.AreEqual(0, pmm1.GetCalculateOrder(model2));
-            Assert.AreEqual(1, pmm1.GetCalculateOrder(model1));
+            Assert.AreEqual((byte)0, pmm1.GetRenderOrder(model2).Value);
+            Assert.AreEqual((byte)1, pmm1.GetRenderOrder(model1).Value);
+            Assert.AreEqual((byte)0, pmm1.GetCalculateOrder(model2).Value);
+            Assert.AreEqual((byte)1, pmm1.GetCalculateOrder(model1).Value);
 
             // モデルが削除されたらそれが反映されるか
             // 存在しないモデルの順序の get/set できちんと例外が投げられるか
             pmm1.Models.Clear();
-            Assert.ThrowsException<KeyNotFoundException>(() => pmm1.GetRenderOrder(model1));
-            Assert.ThrowsException<KeyNotFoundException>(() => pmm1.GetCalculateOrder(model2));
+            Assert.IsNull(pmm1.GetRenderOrder(model1));
+            Assert.IsNull(pmm1.GetCalculateOrder(model2));
             Assert.ThrowsException<ArgumentException>(() => pmm1.SetRenderOrder(model1, 0));
             Assert.ThrowsException<ArgumentException>(() => pmm1.SetCalculateOrder(model2, 0));
 
@@ -77,27 +77,27 @@ namespace UnitTest
             // ちゃんと反映されているか
             pmm1.Models.Add(model1);
             pmm1.Models.Add(model2);
-            Assert.AreEqual(0, pmm1.GetRenderOrder(model1));
-            Assert.AreEqual(1, pmm1.GetRenderOrder(model2));
+            Assert.AreEqual((byte)0, pmm1.GetRenderOrder(model1).Value);
+            Assert.AreEqual((byte)1, pmm1.GetRenderOrder(model2).Value);
 
             pmm1.Models.Remove(model1);
-            Assert.ThrowsException<KeyNotFoundException>(() => pmm1.GetRenderOrder(model1));
-            Assert.AreEqual(0, pmm1.GetCalculateOrder(model2));
+            Assert.ThrowsException<KeyNotFoundException>(() => pmm1.GetRenderOrder(model1).Value);
+            Assert.AreEqual((byte)0, pmm1.GetCalculateOrder(model2).Value);
 
             // モデルの差し替えが発生したとき順序が保たれるか
-            Assert.AreEqual(0, pmm2.GetRenderOrder(model2));
-            Assert.AreEqual(1, pmm2.GetRenderOrder(model1));
-            Assert.AreEqual(0, pmm2.GetCalculateOrder(model2));
-            Assert.AreEqual(1, pmm2.GetCalculateOrder(model1));
+            Assert.AreEqual((byte)0, pmm2.GetRenderOrder(model2).Value);
+            Assert.AreEqual((byte)1, pmm2.GetRenderOrder(model1).Value);
+            Assert.AreEqual((byte)0, pmm2.GetCalculateOrder(model2).Value);
+            Assert.AreEqual((byte)1, pmm2.GetCalculateOrder(model1).Value);
 
             var model3 = new PmmModel() { Name = "モデル3" };
             pmm2.Models[0] = model3;
-            Assert.AreEqual(0, pmm2.GetRenderOrder(model3));
-            Assert.ThrowsException<KeyNotFoundException>(() => pmm2.GetRenderOrder(model2));
-            Assert.AreEqual(1, pmm2.GetRenderOrder(model1));
-            Assert.AreEqual(0, pmm2.GetCalculateOrder(model3));
-            Assert.ThrowsException<KeyNotFoundException>(() => pmm2.GetCalculateOrder(model2));
-            Assert.AreEqual(1, pmm2.GetCalculateOrder(model1));
+            Assert.AreEqual((byte)0, pmm2.GetRenderOrder(model3).Value);
+            Assert.IsNull(pmm2.GetRenderOrder(model2));
+            Assert.AreEqual((byte)1, pmm2.GetRenderOrder(model1).Value);
+            Assert.AreEqual((byte)0, pmm2.GetCalculateOrder(model3).Value);
+            Assert.IsNull(pmm2.GetCalculateOrder(model2));
+            Assert.AreEqual((byte)1, pmm2.GetCalculateOrder(model1).Value);
         }
 
         [TestMethod]
@@ -113,51 +113,51 @@ namespace UnitTest
             pmm1.Accessories.Add(acs2);
 
             // 追加した順に設定される
-            Assert.AreEqual(0, pmm1.GetRenderOrder(acs1));
-            Assert.AreEqual(1, pmm1.GetRenderOrder(acs2));
+            Assert.AreEqual((byte)0, pmm1.GetRenderOrder(acs1).Value);
+            Assert.AreEqual((byte)1, pmm1.GetRenderOrder(acs2).Value);
 
             // 同じアクセサリーが違う PMM に追加されても変わらない
             // 初期実装だとこれがダメだった
             pmm2.Accessories.Add(acs2);
             pmm2.Accessories.Add(acs1);
 
-            Assert.AreEqual(0, pmm2.GetRenderOrder(acs2));
-            Assert.AreEqual(1, pmm2.GetRenderOrder(acs1));
+            Assert.AreEqual((byte)0, pmm2.GetRenderOrder(acs2).Value);
+            Assert.AreEqual((byte)1, pmm2.GetRenderOrder(acs1).Value);
 
-            Assert.AreEqual(0, pmm1.GetRenderOrder(acs1));
-            Assert.AreEqual(1, pmm1.GetRenderOrder(acs2));
+            Assert.AreEqual((byte)0, pmm1.GetRenderOrder(acs1).Value);
+            Assert.AreEqual((byte)1, pmm1.GetRenderOrder(acs2).Value);
 
             // 順序設定の反映がされているか
             pmm1.SetRenderOrder(acs1, 1);
 
-            Assert.AreEqual(0, pmm1.GetRenderOrder(acs2));
-            Assert.AreEqual(1, pmm1.GetRenderOrder(acs1));
+            Assert.AreEqual((byte)0, pmm1.GetRenderOrder(acs2).Value);
+            Assert.AreEqual((byte)1, pmm1.GetRenderOrder(acs1).Value);
 
             // アクセサリーが削除されたらそれが反映されるか
             // 存在しないアクセサリーの順序の get/set できちんと例外が投げられるか
             pmm1.Accessories.Clear();
-            Assert.ThrowsException<KeyNotFoundException>(() => pmm1.GetRenderOrder(acs1));
+            Assert.IsNull(pmm1.GetRenderOrder(acs1));
             Assert.ThrowsException<ArgumentException>(() => pmm1.SetRenderOrder(acs1, 0));
 
             // 一部のアクセサリーを削除したら番号が繰り上がるか
             // ちゃんと反映されているか
             pmm1.Accessories.Add(acs1);
             pmm1.Accessories.Add(acs2);
-            Assert.AreEqual(0, pmm1.GetRenderOrder(acs1));
-            Assert.AreEqual(1, pmm1.GetRenderOrder(acs2));
+            Assert.AreEqual((byte)0, pmm1.GetRenderOrder(acs1).Value);
+            Assert.AreEqual((byte)1, pmm1.GetRenderOrder(acs2).Value);
 
             pmm1.Accessories.Remove(acs1);
-            Assert.ThrowsException<KeyNotFoundException>(() => pmm1.GetRenderOrder(acs1));
+            Assert.IsNull(pmm1.GetRenderOrder(acs1));
 
             // アクセサリーの差し替えが発生したとき順序が保たれるか
-            Assert.AreEqual(0, pmm2.GetRenderOrder(acs2));
-            Assert.AreEqual(1, pmm2.GetRenderOrder(acs1));
+            Assert.AreEqual((byte)0, pmm2.GetRenderOrder(acs2).Value);
+            Assert.AreEqual((byte)1, pmm2.GetRenderOrder(acs1).Value);
 
             var acs3 = new PmmAccessory() { Name = "アクセサリー3" };
             pmm2.Accessories[0] = acs3;
-            Assert.AreEqual(0, pmm2.GetRenderOrder(acs3));
-            Assert.ThrowsException<KeyNotFoundException>(() => pmm2.GetRenderOrder(acs2));
-            Assert.AreEqual(1, pmm2.GetRenderOrder(acs1));
+            Assert.AreEqual((byte)0, pmm2.GetRenderOrder(acs3).Value);
+            Assert.IsNull(pmm2.GetRenderOrder(acs2));
+            Assert.AreEqual((byte)1, pmm2.GetRenderOrder(acs1).Value);
         }
     }
 }
