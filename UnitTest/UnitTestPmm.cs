e4ab3cd4ed257bf8fc1fc478c0863cb5a3f471df
Inwerwm <saclainvervm@gmail.com>
2021-11-06 16:16:21 +0900

    アクセサリーの順序テストを実装

--- UnitTest/UnitTestPmm.cs
+++ UnitTest/UnitTestPmm.cs
@@ -24,7 +24,7 @@ namespace UnitTest
         }
 
         [TestMethod]
-        public void OrderTest()
+        public void ModelOrderTest()
         {
             var pmm1 = new PolygonMovieMaker();
             var pmm2 = new PolygonMovieMaker();
@@ -98,6 +98,66 @@ namespace UnitTest
             Assert.AreEqual(0, pmm2.GetCalculateOrder(model3));
             Assert.ThrowsException<KeyNotFoundException>(() => pmm2.GetCalculateOrder(model2));
             Assert.AreEqual(1, pmm2.GetCalculateOrder(model1));
+        }
+
+        [TestMethod]
+        public void AccessoryOrderTest()
+        {
+            var pmm1 = new PolygonMovieMaker();
+            var pmm2 = new PolygonMovieMaker();
+
+            var acs1 = new PmmAccessory() { Name = "アクセサリー1" };
+            var acs2 = new PmmAccessory() { Name = "アクセサリー2" };
+
+            pmm1.Accessories.Add(acs1);
+            pmm1.Accessories.Add(acs2);
+
+            // 追加した順に設定される
+            Assert.AreEqual(0, pmm1.GetRenderOrder(acs1));
+            Assert.AreEqual(1, pmm1.GetRenderOrder(acs2));
+
+            // 同じアクセサリーが違う PMM に追加されても変わらない
+            // 初期実装だとこれがダメだった
+            pmm2.Accessories.Add(acs2);
+            pmm2.Accessories.Add(acs1);
+
+            Assert.AreEqual(0, pmm2.GetRenderOrder(acs2));
+            Assert.AreEqual(1, pmm2.GetRenderOrder(acs1));
+
+            Assert.AreEqual(0, pmm1.GetRenderOrder(acs1));
+            Assert.AreEqual(1, pmm1.GetRenderOrder(acs2));
+
+            // 順序設定の反映がされているか
+            pmm1.SetRenderOrder(acs1, 1);
+
+            Assert.AreEqual(0, pmm1.GetRenderOrder(acs2));
+            Assert.AreEqual(1, pmm1.GetRenderOrder(acs1));
+
+            // アクセサリーが削除されたらそれが反映されるか
+            // 存在しないアクセサリーの順序の get/set できちんと例外が投げられるか
+            pmm1.Accessories.Clear();
+            Assert.ThrowsException<KeyNotFoundException>(() => pmm1.GetRenderOrder(acs1));
+            Assert.ThrowsException<ArgumentException>(() => pmm1.SetRenderOrder(acs1, 0));
+
+            // 一部のアクセサリーを削除したら番号が繰り上がるか
+            // ちゃんと反映されているか
+            pmm1.Accessories.Add(acs1);
+            pmm1.Accessories.Add(acs2);
+            Assert.AreEqual(0, pmm1.GetRenderOrder(acs1));
+            Assert.AreEqual(1, pmm1.GetRenderOrder(acs2));
+
+            pmm1.Accessories.Remove(acs1);
+            Assert.ThrowsException<KeyNotFoundException>(() => pmm1.GetRenderOrder(acs1));
+
+            // アクセサリーの差し替えが発生したとき順序が保たれるか
+            Assert.AreEqual(0, pmm2.GetRenderOrder(acs2));
+            Assert.AreEqual(1, pmm2.GetRenderOrder(acs1));
+
+            var acs3 = new PmmAccessory() { Name = "アクセサリー3" };
+            pmm2.Accessories[0] = acs3;
+            Assert.AreEqual(0, pmm2.GetRenderOrder(acs3));
+            Assert.ThrowsException<KeyNotFoundException>(() => pmm2.GetRenderOrder(acs2));
+            Assert.AreEqual(1, pmm2.GetRenderOrder(acs1));
         }
     }
 }
