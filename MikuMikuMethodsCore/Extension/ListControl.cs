78659f872106e4922062dfdd06c3ad39e424559f
Inwerwm <saclainvervm@gmail.com>
2021-11-06 17:02:38 +0900

    リスト内の要素を移動する拡張メソッドを実装

---
+++ MikuMikuMethodsCore/Extension/ListControl.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.Extension
+{
+    internal static class ListControl
+    {
+        public static void Move<T>(this IList<T> list, int index, T item) where T : class
+        {
+            if (list == null) return;
+            if (!list.Any()) return;
+            if(list[index] == item) return;
+
+            if (list.Remove(item))
+                list.Insert(index, item);
+        }
+    }
+}
