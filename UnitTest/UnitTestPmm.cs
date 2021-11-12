c9e424d8f7b270b3710e358b034003ce36a009a9
Inwerwm <saclainvervm@gmail.com>
2021-11-12 20:52:34 +0900

    PMMのテストを集約

--- UnitTest/UnitTestPmm.cs
+++ UnitTest/UnitTestPmm.cs
@@ -1,12 +1,7 @@
 ﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
 using MikuMikuMethods.PMM;
+using MikuMikuMethods.PMM.Frame;
 using System;
-using System.Collections.Generic;
-using System.IO;
-using System.Linq;
-using System.Text;
-using System.Text.Json;
-using System.Threading.Tasks;
 
 namespace UnitTest
 {
@@ -158,6 +153,17 @@ namespace UnitTest
             Assert.AreEqual((byte)0, pmm2.GetRenderOrder(acs3).Value);
             Assert.IsNull(pmm2.GetRenderOrder(acs2));
             Assert.AreEqual((byte)1, pmm2.GetRenderOrder(acs1).Value);
+        }
+
+        [TestMethod]
+        public void Test_Asc_TransAndVisible()
+        {
+            var tv = PmmAccessoryFrame.SeparateTransAndVisible(45);
+            var re = PmmAccessoryFrame.CreateTransAndVisible(tv.Transparency, tv.Visible);
+
+            Assert.IsTrue(tv.Visible);
+            Assert.AreEqual(0.78f, tv.Transparency);
+            Assert.AreEqual(45, re);
         }
     }
 }
