685d6307d0360d7b69c54f91356e160eb24dbed0
Inwerwm <saclainvervm@gmail.com>
2021-11-14 22:21:16 +0900

    コンストラクタを作成し、外部非公開に変更

--- MikuMikuMethodsCore/PMM/PmmNode.cs
+++ MikuMikuMethodsCore/PMM/PmmNode.cs
@@ -2,11 +2,17 @@
 
 namespace MikuMikuMethods.Pmm;
 
-public record PmmNode
+
+public class PmmNode
 {
-    public string Name { get; set; }
+    public string Name { get; internal set; }
     public bool doesOpen { get; set; } = false;
     public ImmutableArray<IPmmModelElement> Elements { get; init; } = new();
 
     public override string ToString() => Name;
+
+    internal PmmNode()
+    {
+        Name = "";
+    }
 }
