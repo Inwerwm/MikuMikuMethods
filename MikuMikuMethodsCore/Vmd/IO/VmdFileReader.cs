bc1e6ab0dbf2bac206d07a5b7317e0628fd355b8
Inwerwm <saclainvervm@gmail.com>
2021-11-20 12:59:32 +0900

    VMDの読み書きクラスを作成

---
+++ MikuMikuMethodsCore/Vmd/IO/VmdFileReader.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.Vmd.IO;
+
+internal static class VmdFileReader
+{
+    public static void Read(VocaloidMotionData vmd)
+    {
+
+    }
+}
