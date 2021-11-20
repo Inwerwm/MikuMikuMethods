84aa6659475d00ad3a2c810c6523d5e38f8e50bd
Inwerwm <saclainvervm@gmail.com>
2021-11-20 15:56:45 +0900

    異なるファイル体系間の連携は専用名前空間を作ってやることにしたのでクラスは削除する

--- MikuMikuMethodsCore/Extension/Adapter.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace MikuMikuMethods.Extension;
-
-static class Adapter
-{
-    public static Pmm.PmmModel CreateFrom(Pmx.PmxModel pmx, string modelPath)
-    {
-        throw new NotImplementedException();
-    }
-}
