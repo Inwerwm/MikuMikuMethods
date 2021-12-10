5ef3defbeee3add5e854c95d26d6ea64b9364dc6
Inwerwm <saclainvervm@gmail.com>
2021-12-10 10:56:34 +0900

    ドキュメントコメントを追加

--- MikuMikuMethodsCore/Converter/Options.cs
+++ MikuMikuMethodsCore/Converter/Options.cs
@@ -6,4 +6,12 @@ using System.Threading.Tasks;
 
 namespace MikuMikuMethods.Converter;
 
+/// <summary>
+/// PMMデータからカメラ情報を抽出する際のオプション
+/// </summary>
+/// <param name="ExtractCamera">カメラフレームを抽出する</param>
+/// <param name="ExtractLight">照明フレームを抽出する</param>
+/// <param name="ExtractShadow">影フレームを抽出する</param>
+/// <param name="StartFrame">抽出開始フレーム</param>
+/// <param name="EndFrame">抽出終了フレーム nullなら全フレーム</param>
 public sealed record ExtractCameraMotionOptions(bool ExtractCamera = true, bool ExtractLight = false, bool ExtractShadow = false, uint StartFrame = 0, uint? EndFrame = null);
