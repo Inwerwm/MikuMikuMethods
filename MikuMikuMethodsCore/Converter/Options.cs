0643647fe62beae7105b3d08bb5293e17aa86d7b
Inwerwm <saclainvervm@gmail.com>
2021-12-12 13:45:48 +0900

    カメラオプションのシグネチャの順序を変更

--- MikuMikuMethodsCore/Converter/Options.cs
+++ MikuMikuMethodsCore/Converter/Options.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,9 +9,10 @@ namespace MikuMikuMethods.Converter;
 /// <summary>
 /// PMMデータからカメラ情報を抽出する際のオプション
 /// </summary>
+/// <param name="StartFrame">抽出開始フレーム</param>
+/// <param name="EndFrame">抽出終了フレーム nullなら全フレーム</param>
 /// <param name="ExtractCamera">カメラフレームを抽出する</param>
 /// <param name="ExtractLight">照明フレームを抽出する</param>
 /// <param name="ExtractShadow">影フレームを抽出する</param>
-/// <param name="StartFrame">抽出開始フレーム</param>
-/// <param name="EndFrame">抽出終了フレーム nullなら全フレーム</param>
-public sealed record ExtractCameraMotionOptions(bool ExtractCamera = true, bool ExtractLight = false, bool ExtractShadow = false, uint StartFrame = 0, uint? EndFrame = null);
+public sealed record ExtractCameraMotionOptions(uint StartFrame = 0, uint? EndFrame = null, bool ExtractCamera = true, bool ExtractLight = false, bool ExtractShadow = false);
+
