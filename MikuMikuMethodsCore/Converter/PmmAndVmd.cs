adc0b59ab1c055c8f59de8f0d33cdbb3e2d05e03
Inwerwm <saclainvervm@gmail.com>
2021-12-12 14:33:03 +0900

    PMM からモーション VMD の抽出処理を実装

--- MikuMikuMethodsCore/Converter/PmmAndVmd.cs
+++ MikuMikuMethodsCore/Converter/PmmAndVmd.cs
@@ -1,4 +1,4 @@
-using MikuMikuMethods.Pmm;
+﻿using MikuMikuMethods.Pmm;
 using MikuMikuMethods.Pmm.Frame;
 using MikuMikuMethods.Vmd;
 using System;
@@ -38,6 +38,23 @@ public static class PmmAndVmd
         return vmd;
     }
 
+    public static VocaloidMotionData ExtractModelMotion(this PmmModel model, ExtractModelMotionOptions? options = default)
+    {
+        // 引数の既定値ではコンパイル時定数しか無理なのでここで null 時の規定値を入れる
+        if (options is null) options = new();
+
+        // ラムダ式でキャプチャしないと options が not null であることがフロー解析で確定できない
+        Func<IPmmFrame, bool> isFrameInRange = frame => options.StartFrame <= frame.Frame && frame.Frame <= (options.EndFrame ?? uint.MaxValue);
+
+        return new()
+        {
+            ModelName = model.Name,
+            MotionFrames = model.Bones.SelectMany(bone => bone.Frames.Where(isFrameInRange).Select(f => ((PmmBoneFrame)f).ToVmdFrame(bone.Name))).ToList(),
+            MorphFrames = model.Morphs.SelectMany(morph => morph.Frames.Where(isFrameInRange).Select(f => ((PmmMorphFrame)f).ToVmdFrame(morph.Name))).ToList(),
+            PropertyFrames = model.ConfigFrames.Where(isFrameInRange).Select(f => ((PmmModelConfigFrame)f).ToVmdFrame()).ToList(),
+        };
+    }
+
     public static VmdCameraFrame ToVmdFrame(this PmmCameraFrame frame) => new((uint)frame.Frame)
     {
         Distance = frame.Distance,
@@ -58,5 +75,23 @@ public static class PmmAndVmd
     {
         Mode = (byte)frame.ShadowMode,
         Range = frame.ShadowRange,
+    };
+
+    public static VmdMotionFrame ToVmdFrame(this PmmBoneFrame frame, string boneName) => new(boneName ,(uint)frame.Frame)
+    {
+        Position = frame.Movement,
+        Rotation = frame.Rotation,
+        InterpolationCurves = new(frame.InterpolationCurves),
+    };
+
+    public static VmdMorphFrame ToVmdFrame(this PmmMorphFrame frame, string morphName) => new(morphName, (uint)frame.Frame)
+    {
+        Weight = frame.Weight,
+    };
+
+    public static VmdPropertyFrame ToVmdFrame(this PmmModelConfigFrame frame) => new((uint)frame.Frame)
+    {
+        IsVisible = frame.Visible,
+        IKEnabled = frame.EnableIK.ToDictionary(p => p.Key.Name, p => p.Value),
     };
 }
