c73573acd811682f9ff987ad71ced94297487856
Inwerwm <saclainvervm@gmail.com>
2021-12-12 13:48:12 +0900

    モデルモーション用オプションを追加

--- MikuMikuMethodsCore/Converter/Options.cs
+++ MikuMikuMethodsCore/Converter/Options.cs
@@ -1,4 +1,4 @@
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -16,3 +16,9 @@ namespace MikuMikuMethods.Converter;
 /// <param name="ExtractShadow">影フレームを抽出する</param>
 public sealed record ExtractCameraMotionOptions(uint StartFrame = 0, uint? EndFrame = null, bool ExtractCamera = true, bool ExtractLight = false, bool ExtractShadow = false);
 
+/// <summary>
+/// PMMデータからモデルモーション情報を抽出する際のオプション
+/// </summary>
+/// <param name="StartFrame">抽出開始フレーム</param>
+/// <param name="EndFrame">抽出終了フレーム nullなら全フレーム</param>
+public sealed record ExtractModelMotionOptions(uint StartFrame = 0, uint? EndFrame = null);
