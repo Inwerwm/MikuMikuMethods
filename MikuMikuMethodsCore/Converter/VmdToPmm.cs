e883c7fc53b92b8ed12ae14abc5678706f0a3a3e
Inwerwm <saclainvervm@gmail.com>
2022-02-21 11:28:37 +0900

    PMMにVMDを適用するメソッドのシグネチャ作成と引数チェックの実装

---
+++ MikuMikuMethodsCore/Converter/VmdToPmm.cs
@@ -0,0 +1,25 @@
+﻿using MikuMikuMethods.Pmm;
+using MikuMikuMethods.Vmd;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MikuMikuMethods.Converter;
+public static class VmdToPmm
+{
+    public static void ApplyVmd(this PolygonMovieMaker pmm, VocaloidMotionData cameraVmd)
+    {
+        if (cameraVmd.Kind != VmdKind.Camera) throw new ArgumentException("The Model VMD was passed as the argument where the Camera VMD was expected.");
+
+
+    }
+
+    public static void ApplyVmd(this PmmModel model, VocaloidMotionData motionVmd)
+    {
+        if (motionVmd.Kind != VmdKind.Model) throw new ArgumentException("The Camera VMD was passed as the argument where the Model VMD was expected.");
+
+
+    }
+}
