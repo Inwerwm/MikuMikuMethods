04bfe93bb72418534198f7cc2258e13258a7d247
Inwerwm <saclainvervm@gmail.com>
2022-02-21 15:50:26 +0900

    抽出オプションのレコードを流用してどの項目を適用するかを選択できるようにした。

--- MikuMikuMethodsCore/Converter/ApplyVmd.cs
+++ MikuMikuMethodsCore/Converter/ApplyVmd.cs
@@ -1,50 +1,81 @@
 ﻿using MikuMikuMethods.Pmm;
 using MikuMikuMethods.Pmm.Frame;
 using MikuMikuMethods.Vmd;
-using MikuMikuMethods.Common;
 
 namespace MikuMikuMethods.Converter;
 public static class ApplyVmd
 {
-    public static void ApplyCameraVmd(this PolygonMovieMaker pmm, VocaloidMotionData cameraVmd)
+    public static void ApplyCameraVmd(this PolygonMovieMaker pmm, VocaloidMotionData cameraVmd, ExtractCameraMotionOptions? options = default)
     {
         if (cameraVmd.Kind != VmdKind.Camera) throw new ArgumentException("The Model VMD was passed as the argument where the Camera VMD was expected.");
 
-        foreach (var frame in cameraVmd.CameraFrames)
+        if (options is null) options = new ExtractCameraMotionOptions()
         {
-            pmm.Camera.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+            ExtractCamera = true,
+            ExtractLight = true,
+            ExtractShadow = true,
+        };
+
+        if (options.ExtractCamera)
+        {
+            foreach (var frame in cameraVmd.CameraFrames)
+            {
+                pmm.Camera.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+            }
         }
 
-        foreach (var frame in cameraVmd.LightFrames)
+        if (options.ExtractLight)
         {
-            pmm.Light.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+            foreach (var frame in cameraVmd.LightFrames)
+            {
+                pmm.Light.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+            }
         }
 
-        foreach (var frame in cameraVmd.ShadowFrames)
+        if (options.ExtractShadow)
         {
-            pmm.SelfShadow.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+            foreach (var frame in cameraVmd.ShadowFrames)
+            {
+                pmm.SelfShadow.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+            }
         }
     }
 
-    public static void ApplyModelVmd(this PmmModel model, VocaloidMotionData modelVmd)
+    public static void ApplyModelVmd(this PmmModel model, VocaloidMotionData modelVmd, ExtractModelMotionOptions? options = default)
     {
         if (modelVmd.Kind != VmdKind.Model) throw new ArgumentException("The Camera VMD was passed as the argument where the Model VMD was expected.");
 
-        foreach (var frame in modelVmd.MotionFrames)
+        if (options is null) options = new ExtractModelMotionOptions()
         {
-            var targetBone = model.Bones.FirstOrDefault(bone => bone.Name == frame.Name);
-            targetBone?.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+            ExtractMotion = true,
+            ExtractMorph = true,
+            ExtractProperty = true,
+        };
+
+        if (options.ExtractMotion)
+        {
+            foreach (var frame in modelVmd.MotionFrames)
+            {
+                var targetBone = model.Bones.FirstOrDefault(bone => bone.Name == frame.Name);
+                targetBone?.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+            }
         }
 
-        foreach (var frame in modelVmd.MorphFrames)
+        if (options.ExtractMorph)
         {
-            var targetMorph = model.Morphs.FirstOrDefault(morph => morph.Name == frame.Name);
-            targetMorph?.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+            foreach (var frame in modelVmd.MorphFrames)
+            {
+                var targetMorph = model.Morphs.FirstOrDefault(morph => morph.Name == frame.Name);
+                targetMorph?.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+            }
         }
 
-        foreach (var frame in modelVmd.PropertyFrames)
+        if (options.ExtractProperty)
         {
-            model.ConfigFrames.AddOrOverWrite(frame.ToPmmFrame(model.Bones), (left, right) => left.Frame == right.Frame);
+            foreach (var frame in modelVmd.PropertyFrames)
+            {
+                model.ConfigFrames.AddOrOverWrite(frame.ToPmmFrame(model.Bones), (left, right) => left.Frame == right.Frame);
+            }
         }
     }
 
