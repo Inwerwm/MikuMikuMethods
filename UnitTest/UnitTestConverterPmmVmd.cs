3e409ec4390638e5b54acc6039f77cc3de2a05ad
Inwerwm <saclainvervm@gmail.com>
2022-03-02 21:29:11 +0900

    各フレーム同士の等価性比較を行うようにした

--- UnitTest/UnitTestConverterPmmVmd.cs
+++ UnitTest/UnitTestConverterPmmVmd.cs
@@ -2,6 +2,7 @@
 using MikuMikuMethods.Converter;
 using MikuMikuMethods.Mme;
 using MikuMikuMethods.Pmm;
+using MikuMikuMethods.Pmm.Frame;
 using MikuMikuMethods.Vmd;
 
 namespace UnitTest;
@@ -86,15 +87,14 @@ public class UnitTestConverterPmmVmd
         // 期待データのフレーム順をソートしておくことで後々比較できるようにする
         var expected = new PolygonMovieMaker(TestData.GetPath("ApplyExpected.pmm"));
         var expectedMiku = expected.Models[3];
-        SortMotionFrames(expectedMiku.Bones);
         expected.Write(TestData.GetPath("ApplyExpected.pmm"));
 
-        var pmm = new PolygonMovieMaker(TestData.GetPath("ApplyTarget.pmm"));
-        Apply(pmm);
-        pmm.Write(TestData.GetPath("ApplyResult.pmm"));
+        var pmmApply = new PolygonMovieMaker(TestData.GetPath("ApplyTarget.pmm"));
+        var applyMiku = Apply(pmmApply);
+        pmmApply.Write(TestData.GetPath("ApplyResult.pmm"));
 
         var pmmOverWrite = new PolygonMovieMaker(TestData.GetPath("ApplyExpected.pmm"));
-        Apply(pmmOverWrite);
+        var overMiku = Apply(pmmOverWrite);
         pmmOverWrite.Write(TestData.GetPath("ApplyOverwrite.pmm"));
 
         // EMM ファイルをコピーしてテスト後の確認をしやすくしておく
@@ -103,7 +103,36 @@ public class UnitTestConverterPmmVmd
         emm.Write(TestData.GetPath("ApplyOverwrite.emm"));
 
 
-        static void Apply(PolygonMovieMaker pmm)
+        // 各フレーム比較のためフレーム数順でソート
+        var boneFramesGroupedByName = expectedMiku.Bones.Select(bone => (
+            Expected: bone.Frames,
+            Apply: applyMiku.Bones.FirstOrDefault(b => b.Name == bone.Name)?.Frames,
+            Over: overMiku.Bones.FirstOrDefault(b => b.Name == bone.Name)?.Frames
+        ));
+
+        CompareFrames(boneFramesGroupedByName, (frame, makeMsg) =>
+        {
+            Assert.AreEqual(frame.Expected.Movement, frame.Apply.Movement, makeMsg("Apply.Movement is not equal."));
+            Assert.AreEqual(frame.Expected.Movement, frame.Over.Movement, makeMsg("Over.Movement is not equal."));
+
+            // 回転軸制限の関係上、回転は合わせられない
+            //Assert.AreEqual(frame.Expected.Rotation, frame.Apply.Rotation, makeMsg("Apply.Rotation is not equal."));
+            //Assert.AreEqual(frame.Expected.Rotation, frame.Over.Rotation, makeMsg("Over.Rotation is not equal."));
+        });
+
+        var morphFramesGroupedByName = expectedMiku.Morphs.Select(morph => (
+            Expected: morph.Frames,
+            Apply: applyMiku.Morphs.FirstOrDefault(m => m.Name == morph.Name)?.Frames,
+            Over: overMiku.Morphs.FirstOrDefault(m => m.Name == morph.Name)?.Frames
+        ));
+
+        CompareFrames(morphFramesGroupedByName, (frame, makeMsg) =>
+        {
+            Assert.AreEqual(frame.Expected.Weight, frame.Apply.Weight, makeMsg("Apply.Weight is not equal."));
+            Assert.AreEqual(frame.Expected.Weight, frame.Over.Weight, makeMsg("Over.Weight is not equal."));
+        });
+
+        static PmmModel Apply(PolygonMovieMaker pmm)
         {
             var miku = pmm.Models[3];
             pmm.Camera.Frames.Clear();
@@ -118,14 +147,36 @@ public class UnitTestConverterPmmVmd
             pmm.ApplyCameraVmd(cameraVmd);
             miku.ApplyModelVmd(motionVmd);
 
-            SortMotionFrames(miku.Bones);
+            return miku;
         }
 
-        static void SortMotionFrames(IEnumerable<PmmBone> bones)
+        static void SortMotionFrames<T>(List<T> frames) where T : IPmmFrame
         {
-            foreach (var bone in bones)
+            frames.Sort((left, right) => left.Frame.CompareTo(right.Frame));
+        }
+
+        static void CompareFrames<T>(IEnumerable<(List<T> Expected, List<T> Apply, List<T> Over)> framesGroupedByName, Action<(T Expected, T Apply, T Over, int Count), Func<string, string>> comparer) where T : IPmmFrame
+        {
+            foreach (var sameNameFrames in framesGroupedByName)
             {
-                bone.Frames.Sort((left, right) => left.Frame.CompareTo(right.Frame));
+                SortMotionFrames(sameNameFrames.Expected);
+                SortMotionFrames(sameNameFrames.Apply);
+                SortMotionFrames(sameNameFrames.Over);
+            }
+
+            var frames = framesGroupedByName.SelectMany(sameNameFrames =>
+                sameNameFrames.Expected.Zip(sameNameFrames.Apply, sameNameFrames.Over)
+                                       .Select((tp, i) => (Expected: tp.First, Apply: tp.Second, Over: tp.Third, Count: i))
+            );
+
+            foreach (var frame in frames)
+            {
+                var makeMsg = (string str) => $"Count:{frame.Count} - {str}";
+
+                Assert.AreEqual(frame.Expected.Frame, frame.Apply.Frame, makeMsg("Apply.Frame is not equal."));
+                Assert.AreEqual(frame.Expected.Frame, frame.Over.Frame, makeMsg("Over.Frame is not equal."));
+
+                comparer(frame, makeMsg);
             }
         }
     }
