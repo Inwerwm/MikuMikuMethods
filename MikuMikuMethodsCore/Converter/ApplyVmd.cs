86915a356990547238ef764d7ffb1d9817577140
Inwerwm <saclainvervm@gmail.com>
2022-03-05 18:10:32 +0900

    Vmd の適用フレームのオフセットを実装

--- MikuMikuMethodsCore/Converter/ApplyVmd.cs
+++ MikuMikuMethodsCore/Converter/ApplyVmd.cs
@@ -5,7 +5,7 @@ using MikuMikuMethods.Vmd;
 namespace MikuMikuMethods.Converter;
 public static class ApplyVmd
 {
-    public static void ApplyCameraVmd(this PolygonMovieMaker pmm, VocaloidMotionData cameraVmd, CameraMotionOptions? options = default)
+    public static void ApplyCameraVmd(this PolygonMovieMaker pmm, VocaloidMotionData cameraVmd, uint offset = 0, CameraMotionOptions? options = default)
     {
         if (cameraVmd.Kind != VmdKind.Camera) throw new ArgumentException("The Model VMD was passed as the argument where the Camera VMD was expected.");
 
@@ -20,7 +20,7 @@ public static class ApplyVmd
         {
             foreach (var frame in cameraVmd.CameraFrames)
             {
-                pmm.Camera.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+                pmm.Camera.Frames.AddOrOverWrite(frame.ToPmmFrame(offset), (left, right) => left.Frame == right.Frame);
             }
         }
 
@@ -28,7 +28,7 @@ public static class ApplyVmd
         {
             foreach (var frame in cameraVmd.LightFrames)
             {
-                pmm.Light.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+                pmm.Light.Frames.AddOrOverWrite(frame.ToPmmFrame(offset), (left, right) => left.Frame == right.Frame);
             }
         }
 
@@ -36,12 +36,12 @@ public static class ApplyVmd
         {
             foreach (var frame in cameraVmd.ShadowFrames)
             {
-                pmm.SelfShadow.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+                pmm.SelfShadow.Frames.AddOrOverWrite(frame.ToPmmFrame(offset), (left, right) => left.Frame == right.Frame);
             }
         }
     }
 
-    public static void ApplyModelVmd(this PmmModel model, VocaloidMotionData modelVmd, ModelMotionOptions? options = default)
+    public static void ApplyModelVmd(this PmmModel model, VocaloidMotionData modelVmd, uint offset = 0, ModelMotionOptions? options = default)
     {
         if (modelVmd.Kind != VmdKind.Model) throw new ArgumentException("The Camera VMD was passed as the argument where the Model VMD was expected.");
 
@@ -57,7 +57,7 @@ public static class ApplyVmd
             foreach (var frame in modelVmd.MotionFrames)
             {
                 var targetBone = model.Bones.FirstOrDefault(bone => bone.Name == frame.Name);
-                targetBone?.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+                targetBone?.Frames.AddOrOverWrite(frame.ToPmmFrame(offset), (left, right) => left.Frame == right.Frame);
             }
         }
 
@@ -66,7 +66,7 @@ public static class ApplyVmd
             foreach (var frame in modelVmd.MorphFrames)
             {
                 var targetMorph = model.Morphs.FirstOrDefault(morph => morph.Name == frame.Name);
-                targetMorph?.Frames.AddOrOverWrite(frame.ToPmmFrame(), (left, right) => left.Frame == right.Frame);
+                targetMorph?.Frames.AddOrOverWrite(frame.ToPmmFrame(offset), (left, right) => left.Frame == right.Frame);
             }
         }
 
@@ -74,14 +74,14 @@ public static class ApplyVmd
         {
             foreach (var frame in modelVmd.PropertyFrames)
             {
-                model.ConfigFrames.AddOrOverWrite(frame.ToPmmFrame(model.Bones), (left, right) => left.Frame == right.Frame);
+                model.ConfigFrames.AddOrOverWrite(frame.ToPmmFrame(model.Bones, offset), (left, right) => left.Frame == right.Frame);
             }
         }
     }
 
-    private static PmmCameraFrame ToPmmFrame(this VmdCameraFrame frame) => new()
+    private static PmmCameraFrame ToPmmFrame(this VmdCameraFrame frame, uint offset) => new()
     {
-        Frame = (int)frame.Frame,
+        Frame = (int)(frame.Frame + offset),
         Distance = frame.Distance,
         EyePosition = frame.Position,
         Rotation = frame.Rotation,
@@ -90,38 +90,38 @@ public static class ApplyVmd
         InterpolationCurves = frame.InterpolationCurves,
     };
 
-    public static PmmLightFrame ToPmmFrame(this VmdLightFrame frame) => new()
+    public static PmmLightFrame ToPmmFrame(this VmdLightFrame frame, uint offset) => new()
     {
-        Frame = (int)frame.Frame,
+        Frame = (int)(frame.Frame + offset),
         Color = frame.Color,
         Position = frame.Position,
     };
 
-    public static PmmSelfShadowFrame ToPmmFrame(this VmdShadowFrame frame) => new()
+    public static PmmSelfShadowFrame ToPmmFrame(this VmdShadowFrame frame, uint offset) => new()
     {
-        Frame = (int)frame.Frame,
+        Frame = (int)(frame.Frame + offset),
         ShadowMode = frame.Mode,
         ShadowRange = frame.Range,
     };
 
-    public static PmmBoneFrame ToPmmFrame(this VmdMotionFrame frame) => new()
+    public static PmmBoneFrame ToPmmFrame(this VmdMotionFrame frame, uint offset) => new()
     {
-        Frame = (int)frame.Frame,
+        Frame = (int)(frame.Frame + offset),
         Movement = frame.Position,
         Rotation = frame.Rotation,
         InterpolationCurves = new(frame.InterpolationCurves),
         EnablePhysic = true
     };
 
-    public static PmmMorphFrame ToPmmFrame(this VmdMorphFrame frame) => new()
+    public static PmmMorphFrame ToPmmFrame(this VmdMorphFrame frame, uint offset) => new()
     {
-        Frame = (int)frame.Frame,
+        Frame = (int)(frame.Frame + offset),
         Weight = frame.Weight,
     };
 
-    public static PmmModelConfigFrame ToPmmFrame(this VmdPropertyFrame frame, IEnumerable<PmmBone> bones) => new()
+    public static PmmModelConfigFrame ToPmmFrame(this VmdPropertyFrame frame, IEnumerable<PmmBone> bones, uint offset) => new()
     {
-        Frame = (int)frame.Frame,
+        Frame = (int)(frame.Frame + offset),
         Visible = frame.IsVisible,
         EnableIK = bones.Where(bone => bone.IsIK).ToDictionary(
             ikBone => ikBone,
