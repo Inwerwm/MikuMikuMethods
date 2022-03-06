ade8ab11bfa6d5e198c493107fb67d77f8b1a917
Inwerwm <saclainvervm@gmail.com>
2022-03-06 22:29:02 +0900

    複雑なモデルの追加テストを実装

--- UnitTest/UnitTestConverterPmmPmx.cs
+++ UnitTest/UnitTestConverterPmmPmx.cs
@@ -22,4 +22,17 @@ public class UnitTestConverterPmmPmx
         TestData.PmmLoggingRead("AddModelByProgram", false);
         TestData.PmmLoggingRead("OneModel", false);
     }
+
+    [TestMethod]
+    public void ComplexModelAddingTest()
+    {
+        PolygonMovieMaker pmm = new(TestData.GetPath("Blank.pmm"));
+
+        string modelPath = TestData.GetPath("ツインテ少女.pmx");
+        PmxModel model = new PmxModel(modelPath);
+
+        pmm.Models.Add(model.ToPmmModel(modelPath));
+
+        pmm.Write(TestData.GetPath("AddComplexModelByProgram.pmm"));
+    }
 }
